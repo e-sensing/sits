5f842b183f51d09cfbac488dc368f3a6ef2ef2c6
vwmaus <maus@iiasa.ac.at>
2017-03-10 16:07:50 +0100

    Include assessment from dtwSat package

--- inst/extdata/examples/sits_assessment_examples.R
+++ inst/extdata/examples/sits_assessment_examples.R
@@ -1,29 +1,15 @@
 library (sits)
 
 # select samples for pasture and savanna
-cerrado.tb <- sits_getdata (file = "./inst/extdata/Samples/cerrado.json")
-
+cerrado.tb <- sits_getdata (file = system.file("extdata/samples/cerrado.json", package="sits"))
 
 # read a pattern table from a JSON file
-patterns.tb <- sits_getdata(file = "./inst/extdata/patterns/patterns_Rodrigo_7classes_6bands.json")
+patterns.tb <- sits_getdata(file = system.file("extdata/patterns/patterns_Rodrigo_7classes_6bands.json", package="sits"))
 
 bands = c("ndvi", "evi")
 results.tb <- sits_TWDTW(cerrado.tb, patterns.tb, bands, alpha= -0.1, beta = 100, theta = 0.5)
 
-area <- 0.216 * 0.216
+assessment <- sits_assess (results.tb)
 
-confusion.mx <- sits_assess (results.tb, area)
+assessment
 
-savanna.tb <- dplyr::filter (cerrado.tb, label == "Savanna")
-
-sits_plot (savanna.tb[1:10,], type = "one_by_one")
-
-sits_plot (savanna.tb[1:50,], type = "allyears")
-
-sits_plot (cerrado.tb[1:200,], type = "together")
-
-clusters_sav.tb <- sits_cluster (savanna.tb[1:50,], type = "dendogram", n_clusters = 4)
-
-cerrado_pat.tb <- sits_patterns(cerrado.tb)
-
-sits_plot(cerrado_pat.tb, type = "patterns")
