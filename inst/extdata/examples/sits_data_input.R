49a555859f0b5e9c1f0e5ec8e9b3d679b296673a
gilbertocamara <gilberto.camara@inpe.br>
2017-06-30 09:21:13 -0300

    demos for LAPIG

--- inst/extdata/examples/sits_data_input.R
+++ inst/extdata/examples/sits_data_input.R
@@ -6,52 +6,26 @@ library(sits)
 # see WTSS paper for more information ("Web Services for Big Data")
 
 URL <- "http://www.dpi.inpe.br/tws/wtss"
-
-long <-  -55.23354
-lat  <- -11.51652
-
 sits_infoWTSS(URL)
 
 # then, configure the WTSS service
 coverage <-  "mod13q1_512"
 bands    <-  c("ndvi", "evi", "nir")
 
-# pick one point as an example
-point.tb <- sits_getdata(longitude = long, latitude = lat, URL = URL, coverage = coverage, bands = bands)
-# select the evi and plot it
-point.tb %>%
-     sits_select (c("evi")) %>%
-     sits_plot()
+samples1.tb <- sits_getdata(file = system.file("extdata/samples/pasture.csv", package="sits"), URL = URL, coverage = coverage, bands = bands)
 
-#smooth the data and put into a new table
-point2.tb <- sits_smooth (point.tb, lambda = 1.0)
+sits_plot(samples1.tb)
 
-# plot the smoothed time series
-point2.tb %>%
-     sits_select ("ndvi") %>%
-     sits_plot()
+clusters.tb <- sits_cluster(samples1.tb, bands = bands, method = "dendogram")
 
-# rename the smoothed time series
-point3.tb <- sits_rename (point2.tb, c("ndvi_smooth", "evi_smooth", "nir_smooth"))
+patterns1.tb <- sits_patterns (samples1.tb)
 
-# merge the raw and smoothed time series and plot the ndvi
-point3.tb %>%
-     sits_merge (point.tb) %>%
-     sits_select (c("ndvi", "ndvi_smooth")) %>%
-     sits_plot()
+sits_plot (patterns1.tb)
 
-samples1.tb <- sits_getdata(file = system.file("extdata/samples/MatoGrosso-examples.csv", package="sits"), URL = URL, coverage = coverage, bands = bands, n_max = 3)
+patterns2.tb <- sits_patterns (samples1.tb, method = "dendogram")
 
-samples1.tb %>%
-     sits_plot(type = "one_by_one")
+sits_plot (patterns2.tb, type= "patterns")
 
-#we have only three bands, now let's get six bands
-bands6    <-  c("ndvi", "evi", "nir", "red", "blue", "mir")
-samples2.tb <- sits_getdata(table = samples1.tb, URL = URL, coverage = coverage, bands = bands6)
+patterns3.tb <- sits_patterns (samples1.tb, method = "dendogram", apply_gam = TRUE)
 
-# read a pattern table from a JSON file
-patterns.tb <- sits_getdata(file = system.file("extdata/patterns/patterns_MatoGrosso.json", package="sits"))
-
-# plot patterns
-sits_plot (patterns.tb, type = "patterns")
-
+sits_plot (patterns3.tb, type= "patterns")
