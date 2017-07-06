c1a443395a049def54ee7007e03f12e3f2334dc6
gilbertocamara <gilberto.camara@inpe.br>
2017-07-06 09:08:46 -0300

    Replaces sits_assess with sits_accuracy

--- R/sits_accuracy.R
+++ R/sits_accuracy.R
@@ -1,9 +1,16 @@
-#' @title Assess time series classification
-#' @name sits_assess
+#' @title Post-classification accuracy assessment of classified maps
+#' @name sits_accuracy
 #' @author Victor Maus, \email{vwmaus1@@gmail.com}
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
-#' @description This function calls \code{\link[dtwSat]{twdtwAssess}} from \pkg{dtwSat}.
+#' @description To use this function, the input table should be a set of results containing
+#' both the label assigned by the user and the classification result.
+#'
+#' We plan to do an improved version of this function that includes a Raster R object
+#' with the classified map and a vector with the labels of the classified map
+#' (Gilberto-Rolf-05-Jun-2017)
+#'
+#' This function calls \code{\link[dtwSat]{twdtwAssess}} from \pkg{dtwSat}.
 #' \code{\link[dtwSat]{twdtwAssess}} performs an accuracy assessment of the classified, including
 #' Overall Accuracy, User's Accuracy, Produce's Accuracy, error matrix (confusion matrix),
 #' and estimated area according to [1-2].
@@ -19,13 +26,14 @@
 #' Good practices for estimating area and assessing accuracy of land change. Remote Sensing of
 #' Environment, 148, pp. 42-57.
 #'
-#' @param results.tb an object of "sits_table" with the results of a time series classification
-#' @param area a numeric vector with the area for each class.
+#' @param results.tb a sits table with a set of lat/long/time locations with known and trusted labels and
+#' a the result of classifications
+#' @param area a list with the area of each label
 #' @param conf.int specifies the confidence level (0-1).
 #' @param rm.nosample if sum of columns and sum of rows of the error matrix are zero
 #' then remove class. Default is TRUE.
 #'@export
-sits_assess <- function (results.tb, area, conf.int = 0.95, rm.nosample = FALSE){
+sits_accuracy <- function (results.tb, area, conf.int = 0.95, rm.nosample = FALSE){
 
      # Get reference classes
      references <- results.tb$label
