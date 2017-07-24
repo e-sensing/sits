a0570f8e4d2b702e7d4617931be313cab20dfe0d
gilbertocamara <gilberto.camara@inpe.br>
2017-07-24 18:37:38 -0300

    new function

--- inst/extdata/examples/sits_filtering_comparison_2.R
+++ inst/extdata/examples/sits_filtering_comparison_2.R
@@ -10,36 +10,62 @@ URL <- "http://www.dpi.inpe.br/tws/wtss"
 wtss_inpe <- sits_infoWTSS(URL)
 
 #select a coverage
-coverage <- "mixl8mod_f"
+coverage <- "mixl8mod"
 
 # get information about a specific coverage
 sits_coverageWTSS(URL,coverage)
 #select the bands used for classification
 bands <- c("ndvi", "evi")
 
-#get the prodes samples
-samples_prodes_f.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.csv", URL = URL,
-                          coverage = coverage, bands = bands)
-#get the full series for each point
-series_prodes_f.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.csv", URL = URL,
-                                  coverage = coverage, bands = bands, ignore_dates = TRUE)
+samples_prodes.tb <-  sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.json")
+series_prodes.tb  <-  sits_getdata(file = "./inst/extdata/samples/prodes_series_226_64.json")
 
-sits_save (samples_prodes_f.tb, json_file = "./inst/extdata/samples/prodes_samples_226_64_f.json")
-sits_save (series_prodes_f.tb, json_file = "./inst/extdata/samples/prodes_series_226_64_f.json")
+series_forest <- dplyr::filter (series_prodes.tb, label == "primary_forest")
 
+sits_plot (series_forest, type = "together")
 #plot the first element of the series
 #sits_plot (series_prodes.tb[606,])
 
-#generate patterns with raw data and plot them
-prodes_patterns_f.tb <- sits_patterns(samples_prodes_f.tb, method = "gam", bands = bands)
-sits_plot(prodes_patterns_f.tb, type = "patterns")
+series_forest %>%
+     sits_envelope (window_size = 3) %>%
+     sits_envelope (window_size = 3) %>%
+     sits_whittaker(lambda = 1.0) %>%
+     sits_select (bands = c("ndvi.upper.lower.whit")) %>%
+     sits_plot (type = "together")
 
+series_cut <- dplyr::filter (series_prodes.tb, label == "clear_cut2015")
+series_cut %>%
+     sits_select (bands = c("ndvi")) %>%
+     sits_plot (type = "together")
+
+series_cut %>%
+     sits_envelope (window_size = 2) %>%
+     sits_envelope (window_size = 2) %>%
+     sits_whittaker(lambda = 1.0) %>%
+     sits_select (bands = c("ndvi.upper.lower.whit")) %>%
+     sits_plot (type = "together")
+
+#generate patterns with raw data and plot them
+prodes_patterns.tb <- sits_patterns(samples_prodes.tb, method = "gam", bands = bands)
+sits_plot(prodes_patterns.tb, type = "patterns")
+
+sample_cut <- dplyr::filter (samples_prodes.tb, label == "clear_cut2015")
+
+sample_cut %>%
+     sits_select (bands = c("ndvi")) %>%
+     sits_plot (type = "together")
 #cross_validate raw series
-cv_raw_f <- sits_cross_validate (samples_prodes_f.tb, method = "gam", bands = bands,
-     times = 50, perc = 0.5, file = "./inst/extdata/validation/cv_raw_f.json")
+cv_raw <- sits_reassess (file = "./inst/extdata/validation/cv_raw.json")
+
+# relabel and see assessment
+prodes_relabel.lst <-  tibble::lst("primary_forest" = "Forest",
+                                   "clear_cut2015"  = "NonForest",
+                                   "clear_cut2016"  = "NonForest",
+                                   "pasture"        = "NonForest")
+cv_raw2 <- sits_reassess (file = "./inst/extdata/validation/cv_raw.json", conv = prodes_relabel.lst )
 
 # test savitsky golay filter
-samples_sg_f.tb <- sits_sgolay(samples_prodes_f.tb)
+seriew_sg.tb <- sits_sgolay(series_prodes.tb)
 bands_sg = c("ndvi.sg", "evi.sg")
 sg_patterns_f <- sits_patterns(samples_sg_f.tb, method = "gam", bands = bands_sg)
 cv_sg <- sits_cross_validate (samples_sg_f.tb, method = "gam", bands = bands_sg,
