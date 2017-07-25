8ef9a730eac6f0706d92483760b6d4f66d7b6b4f
gilbertocamara <gilberto.camara@inpe.br>
2017-07-25 13:47:48 -0300

    Adjustments in sits_relabel and in the filtering examples

--- inst/extdata/examples/sits_filtering_comparison.R
+++ inst/extdata/examples/sits_filtering_comparison.R
@@ -18,17 +18,21 @@ sits_coverageWTSS(URL,coverage)
 bands <- c("ndvi", "evi")
 
 #get the prodes samples
-samples_prodes.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.csv", URL = URL,
-                          coverage = coverage, bands = bands)
+samples_prodes.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.json")
 #get the full series for each point
-series_prodes.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.csv", URL = URL,
-                                  coverage = coverage, bands = bands, ignore_dates = TRUE)
+series_prodes.tb <- sits_getdata(file = "./inst/extdata/samples/prodes_samples_226_64.json")
 
-sits_save (samples_prodes.tb, json_file = "./inst/extdata/samples/prodes_samples_226_64.json")
-sits_save (series_prodes.tb, json_file = "./inst/extdata/samples/prodes_series_226_64.json")
+#relabel the series
+# relabel and see assessment
+prodes_relabel.lst <-  tibble::lst("primary_forest" = "Forest",
+                                   "clear_cut2015"  = "ClearCut",
+                                   "clear_cut2016"  = "ClearCut",
+                                   "pasture"        = "Pasture")
 
-#plot the first element of the series
-sits_plot (series_prodes.tb[606,])
+samples_prodes.tb <- sits_relabel(samples_prodes.tb, prodes_relabel.lst)
+
+#plot the all samples for each class together
+sits_plot (samples_prodes.tb, type = "together")
 
 #generate patterns with raw data and plot them
 prodes_patterns.tb <- sits_patterns(samples_prodes.tb, method = "gam", bands = bands)
@@ -39,9 +43,21 @@ cv_raw <- sits_cross_validate (samples_prodes.tb, method = "gam", bands = bands,
      times = 50, perc = 0.5, file = "./inst/extdata/validation/cv_raw.json")
 
 # test savitsky golay filter
-samples_sg.tb <- sits_sgolay(samples_prodes.tb)
+samples_sg.tb <- sits_sgolay(samples_prodes.tb, order = 2, scale = 1)
+
+#plot the result of SG filter
+sits_plot (samples_sg.tb, type = "together")
 bands_sg = c("ndvi.sg", "evi.sg")
+
+sg1 <- sits_sgolay(samples_prodes.tb[1,], order = 2, scale = 1)
+
+sg1 %>%
+     sits_merge (samples_prodes.tb[1,]) %>%
+     sits_select (bands = c("ndvi", "ndvi.sg")) %>%
+     sits_plot()
+
 sg_patterns <- sits_patterns(samples_sg.tb, method = "gam", bands = bands_sg)
+
 cv_sg <- sits_cross_validate (samples_sg.tb, method = "gam", bands = bands_sg,
                                times = 50, perc = 0.5, file = "./inst/extdata/validation/cv_sg.json")
 
@@ -83,6 +99,11 @@ cv_env_r <- sits_relabel (file = "./inst/extdata/validation/cv_env.json",
 # adjust patterns
 #get MT patterns
 sits_patterns_mt <- sits_getdata (file = "./inst/extdata/patterns/patterns_Damien_Ieda_Rodrigo_15classes_3bands_Water.json")
-sits_forest_pattern.tb <- dplyr::filter(sits_patterns_mt, label == "Forest")
+sits_forest_pattern.tb <- dplyr::filter(sits_patterns_mt, label == "Forest") %>%
+     sits_select(bands = c("ndvi", "evi"))
 
-sits_nonforest.tb <- dplyr::filter(prodes_patterns.tb, label != "primary_forest")
+sits_nonforest.tb <- dplyr::filter(prodes_patterns.tb, label != "Forest")
+
+patterns_mix.tb <- dplyr::bind_rows(sits_forest_pattern.tb, sits_nonforest.tb)
+
+cv_mix <- sits_test_patterns (samples_prodes.tb, patterns_mix.tb, bands = c("ndvi", "evi"))
