f0d5b5ba1877df2da709ca20411745d072fef3a7
vwmaus <maus@iiasa.ac.at>
2017-08-04 12:32:02 +0200

    Create train function for Machine Learning

---
+++ R/sits_machine_learning.R
@@ -0,0 +1,29 @@
+#' @title Train classifiction models using the TWDTW distances for each class
+#' @name sits_train
+#' 
+#' @author Alexandre Xavier Ywata de Carvalho, \email{alexandre.ywata@@ipea.gov.br}
+#' @author Victor Maus, \email{vwmaus1@@gmail.com}
+#' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
+#'
+#' @description Given a SITS tibble time series, returns trained models using Machine Learning.
+#' 
+#' @param data.tb a SITS tibble time series 
+#' 
+#' @export
+sits_train <- function(data.tb, models = "", ...){
+
+     # Create temporal patterns 
+     patterns.tb <- sits_patterns(data.tb, ...)
+     
+     # Apply TWDTW analysis 
+     alignments.tb <- sits_TWDTW_matches(data.tb, patterns.tb, ...)
+     
+     # Spread TWDTW matches  
+     matches.tb <- .sits_spread_matches(alignments.tb)
+     
+     # 
+     
+     
+}
+
+
