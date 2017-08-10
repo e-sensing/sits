701c82068d55a7b0f3e957fa4db4be156ac5fcca
gilbertocamara <gilberto.camara@inpe.br>
2017-08-10 07:24:34 -0300

    new version of vignette

--- inst/extdata/examples/sits_test_machine_learning.R
+++ inst/extdata/examples/sits_test_machine_learning.R
@@ -6,4 +6,23 @@ patterns.tb <- sits_patterns(matogrosso.tb)
 
 matches.tb <- sits_TWDTW_matches(matogrosso.tb, patterns.tb, bands = c("ndvi", "evi"))
 
-model.fit <- sits_train_svm(matches.tb)
+sits_plot(matches.tb[1,], type = "alignments")
+
+# Get best TWDTW aligniments for each class
+matches_distance <- matches.tb[1,] %>%
+    dplyr::rowwise() %>%
+    dplyr::do(twdtw_distances <-
+                  tibble::as_tibble(.$matches[[1]][, c("distance", "label")]) %>%
+                  dplyr::group_by(label) %>%
+                  dplyr::slice(which.min(distance)) %>%
+                  dplyr::ungroup() %>%
+                  dplyr::rename(predicted = label)
+    )
+
+# Select best match and spread pred to columns
+out.tb <- matches.tb[1,] %>%
+    dplyr::bind_cols(matches_distance) %>%
+    tidyr::unnest(twdtw_distances, .drop = FALSE) %>%
+    dplyr::mutate(reference = label) %>%
+    tidyr::spread(predicted, distance)
+
