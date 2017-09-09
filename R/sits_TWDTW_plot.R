01e7294dea1e772383b868a0a7442d3308380d7c
gilbertocamara <gilberto.camara@inpe.br>
2017-09-08 21:48:05 -0300

    plot classification

--- R/sits_TWDTW_plot.R
+++ R/sits_TWDTW_plot.R
@@ -51,10 +51,11 @@ sits_plot_TWDTW_classification <- function (matches, start_date = NULL, end_date
 #'
 #' @description Plots the matches from TWDTW classification for one label
 #'
-#' @param matches      a list of dtwSat S4 matches objects produced by sits_TWDTW_matches
-#' @param n_matches    number of matches of a given label to be displayed
+#' @param matches       a list of dtwSat S4 matches objects produced by sits_TWDTW_matches
+#' @param patterns.tb   a set of known temporal signatures for the chosen classes
+#' @param n_matches     number of matches of a given label to be displayed
 #' @export
-sits_plot_TWDTW_matches <- function (matches, n_matches = 4) {
+sits_plot_TWDTW_matches <- function (matches, patterns.tb, n_matches = 4) {
 
     matches %>%
         purrr::map(function (m.twdtw) {
@@ -62,4 +63,75 @@ sits_plot_TWDTW_matches <- function (matches, n_matches = 4) {
                 graphics::plot()
         })
     return (invisible(matches))
+}
+
+#' @title Plotting subintervals classification
+#' @author Victor Maus, \email{vwmaus1@@gmail.com}
+#'
+#' @description Method for plotting the classification of each
+#' subinterval of the time series based on TWDTW analysis.
+#'
+#' @param x An object of class \code{\link[dtwSat]{twdtwMatches}}.
+#' @param timeseries.labels the label or index of the time series.
+#' Default is 1.
+#' @param patterns.labels a vector with labels of the patterns. If not
+#' declared the function will plot one alignment for each pattern.
+#'
+#' @param attr An \link[base]{integer} vector or \link[base]{character} vector
+#' indicating the attribute for plotting. If not declared the function will plot
+#' all attributes.
+#' @param ... additional arguments passed to \code{\link[dtwSat]{twdtwClassify}}.
+#'
+#' @return A \link[ggplot2]{ggplot} object.
+#' @export
+
+plotClassification <-  function(x, timeseries.labels=NULL, patterns.labels=NULL, attr, ...){
+
+#    if(length(timeseries.labels)>6) timeseries.labels = timeseries.labels[1:6]
+#    x = subset(x, timeseries.labels, patterns.labels)
+
+    ## Get data
+    if(missing(attr)) attr = names(dtwSat::getTimeSeries(x,1)[[1]])
+    df.x = do.call("rbind", lapply(as.list(x), function(xx){
+        ts = dtwSat::getTimeSeries(xx)[[1]][,attr,drop=FALSE]
+        data.frame(Time=index(ts), ts, Series=labels(xx)$timeseries)
+    }))
+    df.x = reshape2::melt(df.x, id.vars=c("Time","Series"))
+
+    y.labels = scales::pretty_breaks()(range(df.x$value, na.rm = TRUE))
+    y.breaks = y.labels
+
+    df.pol = do.call("rbind", lapply(as.list(x), function(xx){
+        best_class = xx[[1]]
+        df.pol = do.call("rbind", lapply(1:nrow(best_class), function(i){
+            data.frame(
+                Time = c(best_class$from[i], best_class$to[i], best_class$to[i], best_class$from[i]),
+                Group = rep(i, 4),
+                Class = rep(as.character(best_class$label[i]), 4),
+                value = rep(range(y.breaks, na.rm = TRUE), each=2))
+        }))
+        df.pol$Group = factor(df.pol$Group)
+        df.pol$Class = factor(df.pol$Class)
+        df.pol$Series = rep(as.character(labels(xx)$timeseries), length(df.pol$Time))
+        df.pol
+    }))
+
+    I = min(df.pol$Time, na.rm = TRUE)-30 <= df.x$Time &
+        df.x$Time <= max(df.pol$Time, na.rm = TRUE)+30
+
+    df.x = df.x[I,,drop=FALSE]
+
+    gp <-  ggplot2::ggplot() +
+        ggplot2::facet_wrap(~Series, scales = "free_x", ncol=1) +
+        ggplot2::geom_polygon(data=df.pol, ggplot2::aes_string(x='Time', y='value', group='Group', fill='Class'), alpha=.7) +
+        ggplot2::scale_fill_brewer(palette="Set3") +
+        ggplot2::geom_line(data=df.x, ggplot2::aes_string(x='Time', y='value', colour='variable')) +
+        ggplot2::scale_y_continuous(expand = c(0, 0), breaks=y.breaks, labels=y.labels) +
+        ggplot2::scale_x_date(breaks=ggplot2::waiver(), labels=ggplot2::waiver()) +
+        ggplot2::theme(legend.position = "bottom") +
+        ggplot2::guides(colour = ggplot2::guide_legend(title = "Bands")) +
+        ggplot2::ylab("Value") +
+        ggplot2::xlab("Time")
+
+    gp
 }
