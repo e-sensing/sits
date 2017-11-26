508e16caffb4f8ae74245e921c94e1aa31b6e6c4
gilbertocamara <gilberto.camara@inpe.br>
2017-11-26 18:30:09 -0200

    improved documentation

--- inst/extdata/examples/sits_machine_learning_comparison_1.R
+++ inst/extdata/examples/sits_machine_learning_comparison_1.R
@@ -7,12 +7,11 @@ library (sits)
 #load a data set for with samples for EMBRAPA data set
 embrapa.tb <- readRDS(system.file ("extdata/time_series/embrapa_mt.rds", package = "sits"))
 
-embrapa1.tb <- sits_select (embrapa.tb, bands = c("ndvi", "evi", "nir", "mir"))
+embrapa.tb <- sits_select (embrapa.tb, bands = c("ndvi", "evi", "nir", "mir"))
 
 results <- list()
 
-# test accuracy of TWDTW to measure distances
-conf_svm1.tb <- sits_kfold_validate(embrapa1.tb, folds = 5, timeline, multicores = 2,
+conf_svm1.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                    tr_method   = sits_svm (kernel = "radial", cost = 10))
 print("==================================================")
 print ("== Confusion Matrix = SVM =======================")
@@ -22,25 +21,10 @@ conf_svm1.mx$name <- "svm_10"
 
 results[[length(results) + 1]] <- conf_svm1.mx
 
-embrapa2.tb <- sits_select (embrapa.tb, bands = c("ndvi", "evi", "nir"))
-
-# test accuracy of TWDTW to measure distances
-conf_svm2.tb <- sits_kfold_validate(embrapa2.tb, folds = 5, timeline, multicores = 2,
-                                         tr_method   = sits_svm (kernel = "radial", cost = 10))
-print("==================================================")
-print ("== Confusion Matrix = SVM =======================")
-conf_svm2.mx <- sits_conf_matrix(conf_svm2.tb)
-
-conf_svm2.mx$name <- "svm_10_3bands"
-
-results[[length(results) + 1]] <- conf_svm2.mx
-
-
-
 # =============== GLM ==============================
 
 # generalized liner model (glm)
-conf_glm.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
+conf_glm.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                         tr_method   = sits_glm())
 
 # print the accuracy of the generalized liner model (glm)
@@ -55,7 +39,7 @@ results[[length(results) + 1]] <- conf_glm.mx
 # =============== RFOR ==============================
 
 # test accuracy of TWDTW to measure distances
-conf_rfor.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
+conf_rfor.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                          tr_method   = sits_rfor ())
 print("==================================================")
 print ("== Confusion Matrix = RFOR =======================")
@@ -67,7 +51,7 @@ results[[length(results) + 1]] <- conf_rfor.mx
 # =============== LDA ==============================
 
 # test accuracy of TWDTW to measure distances
-conf_lda.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
+conf_lda.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                         tr_method   = sits_lda ())
 
 print("==================================================")
@@ -79,7 +63,7 @@ results[[length(results) + 1]] <- conf_lda.mx
 
 # =============== MLR ==============================
 # "multinomial log-linear (mlr)
-conf_mlr.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
+conf_mlr.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                         tr_method   = sits_mlr())
 
 # print the accuracy of the Multinomial log-linear
@@ -92,7 +76,7 @@ results[[length(results) + 1]] <- conf_mlr.mx
 
 # =============== GBM ==============================
 # Gradient Boosting Machine
-conf_gbm.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
+conf_gbm.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
                                         tr_method   = sits_gbm())
 
 # print the accuracy of the Gradient Boosting Machine
@@ -106,17 +90,17 @@ results[[length(results) + 1]] <- conf_gbm.mx
 # =============== SVM full validate ==============================
 
 # test accuracy of TWDTW to measure distances
-conf_svm_full.tb <- sits_kfold_validate(embrapa.tb, folds = 5, timeline, multicores = 2,
-                                        tr_method   = sits_svm (cost = 10, kernel = "radial",
+conf_svm_full.tb <- sits_kfold_validate(embrapa.tb, folds = 5, multicores = 2,
+                                        tr_method   = sits_svm (cost = 100, kernel = "radial",
                                                                 tolerance = 0.001, epsilon = 0.1))
 print("==================================================")
 print ("== Confusion Matrix = SVM =======================")
 conf_svm_full.mx <- sits_conf_matrix(conf_svm_full.tb)
-conf_svm_full.mx$name <- "svm_full"
+conf_svm_full.mx$name <- "svm_cost_100"
 
 results[[length(results) + 1]] <- conf_svm_full.mx
 
 WD = getwd()
 
-sits_toXLSX(results, file = paste0(WD, "/accuracy_embrapa_distance.xlsx"))
+sits_toXLSX(results, file = paste0(WD, "/accuracy_matogrosso.xlsx"))
 
