2320ea4a0c551046e44e0f840571a2e1aca78e25
gilbertocamara <gilberto.camara@inpe.br>
2018-04-28 18:41:41 -0300

    adjustments in sits_raster_classification

--- demo/classify_landsat.R
+++ demo/classify_landsat.R
@@ -3,11 +3,14 @@
 
 message("Processing of a mixed Landsat 8 - MODIS data set")
 message("Processing takes a while, please be patient")
-l8m_222068_evi_file <- paste0("/vsicurl/https://www.dropbox.com/s/51d20d1hci55brj/LC8MOD_evi.tif?raw=1")
-l8m_222068_ndvi_file <- paste0("/vsicurl/https://www.dropbox.com/s/wn6q55kvth5r3y3/LC8MOD_ndvi.tif?raw=1")
-l8m_222068_nir_file <- paste0("/vsicurl/https://www.dropbox.com/s/2r4od7s4zlh383r/LC8MOD_nir.tif?raw=1")
+evi_file <- paste0("/vsicurl/https://www.dropbox.com/s/dykv1o4ut1d09ok/LC8MODIS_222_068_2015_evi.tif?raw=1")
+ndvi_file <- paste0("/vsicurl/https://www.dropbox.com/s/p7z69cjo87xgveu/LC8MODIS_222_068_2015_ndvi.tif?raw=1")
+nir_file <- paste0("/vsicurl/https://www.dropbox.com/s/e8lonfuxn6a205d/LC8MODIS_222_068_2015_nir.tif?raw=1")
+red_file <- paste0("/vsicurl/https://www.dropbox.com/s/7aig90ikflgyuba/LC8MODIS_222_068_2015_red.tif?raw=1")
+mir_file <- paste0("/vsicurl/https://www.dropbox.com/s/wvp7y95gy2y1e4n/LC8MODIS_222_068_2015_swir2.tif?raw=1")
 
-files <- c(l8m_222068_ndvi_file, l8m_222068_evi_file, l8m_222068_nir_file)
+files <- c(ndvi_file, evi_file, red_file, nir_file, mir_file)
+bands <- c("ndvi", "evi", "red", "nir", "mir")
 
 # define the timeline
 
@@ -15,32 +18,23 @@ timeline <- timeline_2000_2017 [timeline_2000_2017 >= lubridate::as_date("2015-0
 timeline <- timeline[1:23]
 
 # create a raster metadata file based on the information about the files
-raster.tb <- sits_coverage(service = "RASTER", name = "L8MOD-222_68_2015-2016",
-                           timeline = timeline, bands = c("ndvi", "evi", "nir"), files = files)
+raster.tb <- sits_coverage(service = "RASTER", name = "L8MOD-222_068_2015-2016",
+                           missing_values = c(0.0, 0.0, 0.0, 0.0, 0.0),
+                           minimum_values = c(0.0, 0.0, 0.0, 0.0, 0.0),
+                           scale_factors  = c(0.0001, 0.0001, 0.0001, 0.001, 0.0001),
+                           timeline = timeline, bands = bands, files = files)
 
-# retrieve the samples from EMBRAPA (used as training sets for classification)
-
-# select a file with 61,000 samples
-samples_file <- paste0("https://www.dropbox.com/s/5llfs8w371u6lng/samples_Cerrado_01042018.rda?raw=1")
-download.file(samples_file, destfile = "./samples_Cerrado_01042018.rda")
-load(file = "./samples_Cerrado_01042018.rda")
-
-# select only the ndvi and evi bands
-samples.tb <- sits_select_bands(samples_Cerrado_01042018.tb, bands = c("ndvi", "evi", "nir"))
-
-# create an SVM model
-svm_model <- sits_train(samples.tb, ml_method = sits_svm(normalize = TRUE))
+# retrieve a pre-built SMV model
+model_file <- paste0("https://www.dropbox.com/s/n4ps99ilmngaqgf/ml_model_Cerrado_25042018_5bands.rda?raw=1")
+dest_file  <- paste0(tempdir(),"/ml_model_Cerrado_25042018_5bands.rda")
+download.file(model_file, destfile = dest_file)
+load(dest_file)
 
 message("Please select memory size avaliable for processing in GB (tipical values 1 - 100)")
 memsize <- as.integer(readline(prompt = "Enter a memsize value: "))
 
-message("Please select number of cores (tipical values 1 - 32)")
-multicores <- as.integer(readline(prompt = "Enter number of cores: "))
-
-
 # classify the raster image
-raster_class.tb <- sits_classify_raster(file = "./L8_MOD_222-068-class", raster.tb, ml_model = svm_model,
-                     memsize = memsize, multicores = multicores)
+raster_class.tb <- sits_classify_raster(file = paste0(tempdir(),"/L8_MOD_222-068-class"), raster.tb, ml_model = svm_model, memsize = memsize)
 
 sits_plot_raster(raster_class.tb[1,], title = "LANDSAT-MODIS-222-068-2015-2016")
 
