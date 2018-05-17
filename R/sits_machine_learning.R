6c750308ae4e1e6b1ac0de9a55eabdd1ac8bf9bb
gilbertocamara <gilberto.camara@inpe.br>
2018-05-17 17:46:06 -0300

    adjustments in liquidSVM ML model

--- R/sits_machine_learning.R
+++ R/sits_machine_learning.R
@@ -415,10 +415,11 @@ sits_lda <- function(data.tb = NULL, normalize = TRUE, formula = sits_formula_lo
 #' @param formula          symbolic description of the model to be fit. SITS offers a set of such formulas (default: sits_svm)
 #' @param threads          number of cores for computing the kernel matrices (0 = uses all cores)
 #' @param partition_choice determines the way the input space is partitioned (0 = disables, 5 = usually best)
-#' @param grid_choice      size of hyper-parameter grid used during training (0 = 10 x 10 grid, 1 = 15 x 15; 2 = 20 x 20)
+#' @param gammas           vector of hyper-parameter used during training for width of kernel
+#' @param c_values         vector of hyper-parameter used during training for regularization
+#' @param folds            number of folds for model validation (default = 5)
 #' @param mc_type          strategy for multiclass classification ("OvA_ls", "AvA_ls", "AvA_hinge")
 #' @param predict.prob     if TRUE a LS-svm will be trained and conditional probs will be estimated
-#' @param folds            number of folds for model validation (default = 5)
 #' @param adaptivity_control  use adaptive search heuristic (default 0 - disables heuristic)
 #' @param random_seed      seed for the random generator (default -1 uses the internal timer)
 #' @param do.select        if TRUE also does the whole selection for this model
@@ -440,9 +441,11 @@ sits_lda <- function(data.tb = NULL, normalize = TRUE, formula = sits_formula_lo
 #'}
 #' @export
 sits_liquid_svm <- function(data.tb = NULL, normalize = TRUE, formula = sits_formula_logref(),
-                            threads = 0, partition_choice = 0, grid_choice = 0, folds = 5,
-                            adaptivity_control = 0, random_seed = -1,
-                            mc_type = "OvA_ls", predict.prob = FALSE, useCells = TRUE, do.select = TRUE, ...) {
+                            threads = 0, partition_choice = 0,
+                            gammas = c(1, 0.04, 0.02, 0.01, 0.005, 0.0025),
+                            c_values = c(1, 10, 100, 200, 500, 1000),
+                            folds = 5, adaptivity_control = 0, random_seed = -1,
+                            mc_type = "AvA_hinge", predict.prob = FALSE, useCells = TRUE, do.select = TRUE, ...) {
 
     # function that returns liquidSVM::svm model based on a sits sample tibble
     result_fun <- function(data.tb){
@@ -482,7 +485,8 @@ sits_liquid_svm <- function(data.tb = NULL, normalize = TRUE, formula = sits_for
         # call liquidSVM::svmMulticlass method and return the trained svm model
         result_svm <- liquidSVM::svmMulticlass(x = formula_svm, y = train_data_DT[,2:ncol(train_data_DT)],
                                                threads = threads, partition_choice = partition_choice,
-                                               grid_choice = grid_choice, folds = folds, mc_type = mc_type,
+                                               gammas = gammas, c_values = c_values,
+                                               folds = folds, mc_type = mc_type,
                                                adaptivity_control = adaptivity_control, random_seed =  random_seed,
                                                predict.prob = predict.prob, do.select = do.select, useCells = useCells)
 
@@ -721,6 +725,7 @@ sits_rfor <- function(data.tb = NULL, num.trees = 2000, ...) {
 #' @param data.tb          time series with the training samples
 #' @param normalize        (boolean) 0 = no normalization, 1 = normalize per band
 #' @param formula          symbolic description of the model to be fit. SITS offers a set of such formulas (default: sits_svm)
+#' @param scale            A logical vector indicating the variables to be scaled.
 #' @param kernel           kernel used in training and predicting (options = linear, polynomial, radial basis, sigmoid)
 #' @param degree           exponential of polynomial type kernel
 #' @param coef0	           parameter needed for kernels of type polynomial and sigmoid (default: 0)
@@ -745,8 +750,8 @@ sits_rfor <- function(data.tb = NULL, num.trees = 2000, ...) {
 #' sits_plot(class.tb)
 #'}
 #' @export
-sits_svm <- function(data.tb = NULL, normalize = TRUE, formula = sits_formula_logref(), kernel = "radial",
-                     degree = 3, coef0 = 0, cost = 10, tolerance = 0.001, epsilon = 0.1, cross = 0, ...) {
+sits_svm <- function(data.tb = NULL, normalize = TRUE, formula = sits_formula_logref(), scale = FALSE,
+                     kernel = "radial", degree = 3, coef0 = 0, cost = 10, tolerance = 0.001, epsilon = 0.1, cross = 0, ...) {
 
     # function that returns e1071::svm model based on a sits sample tibble
     result_fun <- function(data.tb){
