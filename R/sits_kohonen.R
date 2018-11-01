aecf1dbb693247d57ca64d8490b086ef8a21a53c
Pedro R. Andrade <pedro.andrade@inpe.br>
2018-11-01 16:32:46 -0300

    Update R/sits_kohonen.R
    Co-Authored-By: lorenalves <lorenalvesdosantos@gmail.com>

--- R/sits_kohonen.R
+++ R/sits_kohonen.R
@@ -90,7 +90,7 @@ sits_kohonen <- function (data.tb, time_series, bands = NULL, grid_xdim = 25, gr
     cluster_sample <- class_matrix[neurons_]
     result.tb$neuron_label <- cluster_sample
 
-    j=1
+    j = 1
     for (j in 1:length(unique(neurons_labelled)))
     {
         paint_neurons <- which(class_matrix[, 2] == j)
