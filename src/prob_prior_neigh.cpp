2bf530e54da3a06f2fd841d77c21bb7082d8b9dc
gilbertocamara <gilberto.camara@inpe.br>
2019-10-03 04:12:24 -0300

    cleaned Cpp code

--- src/prob_prior_neigh.cpp
+++ src/prob_prior_neigh.cpp
@@ -59,48 +59,6 @@ NumericVector bayes_estimator_class(const NumericMatrix& data,
 
     return result;
 }
-int majority_pixel(const IntegerMatrix& data, const IntegerMatrix& window,
-                             const int& i, const int& j, const int& nclasses) {
-
-    int nrows_d = data.nrow();
-    int ncols_d = data.ncol();
-    int nrows_w = window.nrow();
-    int ncols_w = window.ncol();
-    NumericMatrix neigh(nrows_w, ncols_w);
-
-    for (int k = 0; k < nrows_w; k++) {
-        for (int l = 0; l < ncols_w; l++) {
-            int data_i = i + k - (nrows_w - 1)/2;
-            int data_j = j + l - (ncols_w - 1)/2;
-            // check for boundary conditions
-            if (data_i < 0 || data_i >= nrows_d)
-                data_i = i;
-            if (data_j < 0 || data_j >= ncols_d)
-                data_j = j;
-
-            // update the neighbourhood matrix
-            neigh(k,l) = (int) data(data_i, data_j) * window(k, l);
-        }
-    }
-    IntegerVector hist_neigh(nclasses);
-    for (int k = 0; k < nrows_w; k++)
-        for (int l = 0; l < ncols_w; l++)
-            hist_neigh(neigh(k,l)) =  hist_neigh(neigh(k,l)) + 1;
-
-    return which_max(hist_neigh);
-
-}
-// [[Rcpp::export]]
-IntegerVector majority_smooth(const IntegerVector& neigh, const int& n_classes) {
-
-    int n_values = neigh.length();
-    IntegerVector hist_neigh(n_classes);
-
-    for (int i = 0; i < n_values; i++)
-            hist_neigh(neigh(i)) =  hist_neigh(neigh(i)) + 1;
-
-    return which_max(hist_neigh);
-}
 
 
 
