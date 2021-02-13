ddc7b186fcfb223088ec606c5c43ed961a5766e0
Rolf Simoes <rolfsimoes@gmail.com>
2021-02-13 05:41:53 -0300

    change function name / organize code

--- R/sits_smooth_aux_functions.R
+++ R/sits_smooth_aux_functions.R
@@ -1,5 +1,5 @@
 #' @title Estimate the number of blocks to run .sits_split_cluster
-#' @name .sits_split_probs_blocks_estimate
+#' @name .sits_probs_blocks_size_estimate
 #' @keywords internal
 #'
 #' @param cube         input data cube
@@ -10,7 +10,7 @@
 #'             - multicores theoretical upper bound;
 #'             - block x_size (horizontal) and y_size (vertical)
 #'
-.sits_split_probs_blocks_estimate <- function(cube, multicores, memory) {
+.sits_probs_blocks_size_estimate <- function(cube, multicores, memory) {
 
     # precondition 1 - check if cube has probability data
     assertthat::assert_that(inherits(cube, "probs_cube"),
@@ -51,7 +51,7 @@
     # - multicores theoretical upper bound;
     # - block x_size (horizontal) and y_size (vertical)
     blocks <- list(
-        max_multicores = floor(blocking_factor / memory_factor),
+        # max_multicores = floor(blocking_factor / memory_factor),
         block_x_size = floor(min_block_x_size),
         block_y_size = min(floor(blocking_factor / memory_factor / multicores),
                            y_size)
@@ -60,10 +60,10 @@
     return(blocks)
 }
 #' @title parallel raster brick
-#' @name .sits_split_cluster
+#' @name .sits_map_layer_cluster
 #' @keywords internal
 #'
-#' @description Process chunks of raster brick in parallel.
+#' @description Process chunks of raster brick individually in parallel.
 #'
 #' @param cube               Probability data cube
 #' @param cube_out           Output probability data cube
@@ -80,9 +80,9 @@
 #'
 #' @return  RasterBrick object
 #'
-.sits_split_cluster <- function(cube, cube_out, overlapping_y_size = 0,
-                                func, func_args = NULL, multicores = 1,
-                                memory = 1, ...) {
+.sits_map_layer_cluster <- function(cube, cube_out, overlapping_y_size = 0,
+                                    func, func_args = NULL, multicores = 1,
+                                    memory = 1, ...) {
 
     # precondition 1 - check if cube has probability data
     assertthat::assert_that(inherits(cube, "probs_cube"),
@@ -94,7 +94,26 @@
             msg = ".sits_split_cluster: overlaping rows must be non negative"
     )
 
-    # run on workers...
+    # function to compute blocks grid
+    .sits_compute_blocks <- function(img_y_size,
+                                     block_y_size,
+                                     overlapping_y_size) {
+
+        r1 <- ceiling(seq(1, img_y_size - 1, by = block_y_size))
+        r2 <- c(r1[-1] - 1, img_y_size)
+        ovr_r1 <- c(1, c(r1[-1] - overlapping_y_size))
+        ovr_r2 <- c(r2[-length(r2)] + overlapping_y_size, img_y_size)
+
+        # define each block as a list element
+        mapply(list,
+               r1 = ovr_r1,
+               r2 = ovr_r2,
+               o1 = r1 - ovr_r1 + 1,
+               o2 = r2 - ovr_r1 + 1,
+               SIMPLIFY = FALSE)
+    }
+
+    # function that run on workers...
     .sits_cluster_worker_fun <- function(block, in_file, func, args) {
 
         # open brick
@@ -132,52 +151,8 @@
         filename
     }
 
-    # make snow cluster
-    cl <- NULL
-    if (multicores > 1) {
-        cl <- parallel::makeCluster(multicores)
-        lib_paths <- .libPaths()
-        parallel::clusterExport(cl, "lib_paths", envir = environment())
-        parallel::clusterEvalQ(cl, .libPaths(lib_paths))
-        on.exit(parallel::stopCluster(cl))
-    }
-
-    slider::slide2(cube, cube_out, function(cube_row, out_file_row) {
-
-
-        # compute how many tiles to be computed
-        blocks <- .sits_split_probs_blocks_estimate(cube = cube_row,
-                                                    multicores = multicores,
-                                                    memory = memory)
-
-        # # updates multicores if it is above upper bound limit
-        # multicores <- min(blocks[["max_multicores"]], multicores)
-
-        # for now, only vertical blocks are allowed, i.e 'x_blocks' is 1
-        block_y_size <- blocks[["block_y_size"]]
-
-        # open probability file
-        in_file <- .sits_cube_files(cube_row)
-
-        # retrieve the files to be read and written
-        out_file <- .sits_cube_files(out_file_row)
-
-        # get vertical size
-        img_y_size <- cube_row$nrows
-
-        # compute blocks
-        r1 <- ceiling(seq(1, img_y_size - 1, by = block_y_size))
-        r2 <- c(r1[-1] - 1, img_y_size)
-        ovr_r1 <- c(1, c(r1[-1] - overlapping_y_size))
-        ovr_r2 <- c(r2[-length(r2)] + overlapping_y_size, img_y_size)
-
-        # define blocks
-        blocks <- mapply(list,
-                         r1 = ovr_r1,
-                         r2 = ovr_r2,
-                         o1 = r1 - ovr_r1 + 1,
-                         o2 = r2 - ovr_r1 + 1,
-                         SIMPLIFY = FALSE)
+    # function to call workers clusters and merge its results
+    .sits_call_workers_cluster <- function(in_file, out_file, blocks, cl) {
 
         # apply function to blocks
         if (purrr::is_null(cl)) {
@@ -203,19 +178,58 @@
         on.exit(unlink(tmp_blocks))
 
         # merge to save final result with '...' parameters
-        # if there is only one block
+        # if there is only one block...
         if (length(tmp_blocks) == 1)
             return(suppressWarnings(
                 raster::writeRaster(raster::brick(tmp_blocks),
                                     overwrite = TRUE,
                                     filename = out_file, ...)
             ))
-
+        # ... else call raster::merge.
         suppressWarnings(
             do.call(raster::merge, c(lapply(tmp_blocks, raster::brick),
                                      list(overwrite = TRUE, filename = out_file),
                                      list(...)))
         )
+    }
+
+    # make snow cluster
+    cl <- NULL
+    if (multicores > 1) {
+        cl <- parallel::makeCluster(multicores)
+        lib_paths <- .libPaths()
+        parallel::clusterExport(cl, "lib_paths", envir = environment())
+        parallel::clusterEvalQ(cl, .libPaths(lib_paths))
+        on.exit(parallel::stopCluster(cl))
+    }
+
+    # traverse all tiles
+    slider::slide2(cube, cube_out, function(cube_row, out_file_row) {
+
+        # open probability file
+        in_files <- .sits_cube_files(cube_row)
+
+        # retrieve the files to be read and written
+        out_files <- .sits_cube_files(out_file_row)
+
+        # compute how many tiles to be computed
+        block_size <- .sits_probs_blocks_size_estimate(cube = cube_row,
+                                                       multicores = multicores,
+                                                       memory = memory)
+
+        # # updates multicores if it is above upper bound limit
+        # multicores <- min(blocks[["max_multicores"]], multicores)
+
+        # for now, only vertical blocks are allowed, i.e 'x_blocks' is 1
+        blocks <- .sits_compute_blocks(
+            img_y_size = cube_row$nrows,
+            block_y_size = block_size[["block_y_size"]],
+            overlapping_y_size = overlapping_y_size)
+
+        # traverse all years
+        purrr::map2(in_files, out_files, .sits_call_workers_cluster,
+                    blocks = blocks, cl = cl)
+
     })
 
     return(invisible(cube_out))
