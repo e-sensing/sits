1edc1d47a4891d0cb4f7a7c4da29525820ff286f
Rolf Simoes <rolfsimoes@gmail.com>
2021-03-21 14:30:04 -0300

    improve code style

--- R/sits_aws.R
+++ R/sits_aws.R
@@ -11,30 +11,40 @@
 
     # require package
     if (!requireNamespace("aws.s3", quietly = TRUE)) {
-        stop("Please install package aws.s3", call. = FALSE)
+        stop(".sits_aws_check_access: please install package aws.s3",
+             call. = FALSE)
     }
+
     # check "AWS_ACCESS_KEY_ID" - mandatory one per user
     aws_access_key_id <- Sys.getenv("AWS_ACCESS_KEY_ID")
-    assertthat::assert_that(nchar(aws_access_key_id) > 0,
-            msg = "AWS_ACCESS_KEY_ID environment variable missing"
+    assertthat::assert_that(
+        nchar(aws_access_key_id) > 0,
+        msg = paste(".sits_aws_check_access: AWS_ACCESS_KEY_ID environment",
+                    "variable missing")
     )
+
     # check "AWS_SECRET_ACCESS_KEY" - mandatory one per user
     aws_secret_access_key <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
-    assertthat::assert_that(nchar(aws_secret_access_key) > 0,
-            msg = "AWS_SECRET_ACCESS_KEY environment variable missing"
+    assertthat::assert_that(
+        nchar(aws_secret_access_key) > 0,
+        msg = paste(".sits_aws_check_access: AWS_SECRET_ACCESS_KEY",
+                    "environment variable missing")
     )
+
     # check "AWS_DEFAULT_REGION" - if not available, use the default
     aws_region <- Sys.getenv("AWS_DEFAULT_REGION")
     if (nchar(aws_region) == 0) {
         aws_region <- .sits_config_aws_default_region(source)
         Sys.setenv(AWS_DEFAULT_REGION = aws_region)
     }
+
     # check "AWS_ENDPOINT" - if not available, use the default
     aws_endpoint <- Sys.getenv("AWS_ENDPOINT")
     if (nchar(aws_endpoint) == 0) {
         aws_endpoint <- .sits_config_aws_endpoint(source)
         Sys.setenv(AWS_ENDPOINT = aws_endpoint)
     }
+
     # check "AWS_REQUEST_PAYER" - if not available, use the default
     aws_request_payer <- Sys.getenv("AWS_REQUEST_PAYER")
     if (nchar(aws_request_payer) == 0) {
