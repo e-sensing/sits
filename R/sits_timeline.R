e763d81eb389b2aab72214578a3584043d2b6c2e
Rolf Simoes <rolfsimoes@gmail.com>
2021-03-21 14:35:31 -0300

    improve code style

--- R/sits_timeline.R
+++ R/sits_timeline.R
@@ -1,20 +1,26 @@
 #' @title Obtains the timeline
+#'
 #' @name sits_timeline
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description This function returns the timeline for a given data set, either
 #'              a set of time series or a data cube
+#'
 #' @param  data     either a sits tibble or data cube
+#'
 #' @export
+#'
 sits_timeline <- function(data) {
     # get the meta-type (sits or cube)
     data <- .sits_config_data_meta_type(data)
 
     UseMethod("sits_timeline", data)
 }
-#'
+
 #' @title Obtains the timeline for a set of time series
-#' @rdname sits_timeline
+#' @export
+#'
 sits_timeline.sits <- function(data) {
 
     timeline <- NULL
@@ -27,9 +33,10 @@ sits_timeline.sits <- function(data) {
 
     return(timeline)
 }
-#'
+
 #' @title Obtains the timeline for a raster data cube
-#' @rdname sits_timeline
+#' @export
+#'
 sits_timeline.raster_cube <- function(data) {
 
     timeline_first <-  unique(lubridate::as_date(data$file_info[[1]]$date))
@@ -45,9 +52,10 @@ sits_timeline.raster_cube <- function(data) {
     # return the timeline of the cube
     return(timeline_first)
 }
-#'
+
 #' @title Obtains the timeline for a raster data cube
-#' @rdname sits_timeline
+#' @export
+#'
 sits_timeline.satveg_cube <- function(data) {
 
   # retrieve the time series
@@ -59,9 +67,9 @@ sits_timeline.satveg_cube <- function(data) {
   return(as.Date(ts$Index))
 }
 
-#'
 #' @title Obtains the timeline for a probs data cube
-#' @rdname sits_timeline
+#' @export
+#'
 sits_timeline.probs_cube <- function(data) {
 
     assertthat::assert_that(
@@ -75,8 +83,10 @@ sits_timeline.probs_cube <- function(data) {
     timeline <- c(start_date, end_date)
     return(timeline)
 }
+
 #' @title Obtains the timeline for a classified data cube
-#' @rdname sits_timeline
+#' @export
+#'
 sits_timeline.classified_image <- function(data) {
 
     assertthat::assert_that(
@@ -90,8 +100,11 @@ sits_timeline.classified_image <- function(data) {
     timeline <- c(start_date, end_date)
     return(timeline)
 }
+
 #' @title Check cube timeline against requested start and end dates
+#'
 #' @name .sits_timeline_check_cube
+#'
 #' @keywords internal
 #'
 #' @description Tests if required start and end dates are available in
@@ -102,7 +115,7 @@ sits_timeline.classified_image <- function(data) {
 #' @param end_date        End date of the period.
 #'
 #' @return A vector with corrected start and end dates
-
+#'
 .sits_timeline_check_cube <- function(cube, start_date, end_date) {
 
     # get the timeline
@@ -136,8 +149,11 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Define the information required for classifying time series
+#'
 #' @name .sits_timeline_class_info
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description Time series classification requires a series of steps:
@@ -157,14 +173,17 @@ sits_timeline.classified_image <- function(data) {
 #'
 #' @param  data            Description on the data being classified.
 #' @param  samples         Samples used for training the classification model.
+#'
 #' @return A tibble with the classification information.
+#'
 .sits_timeline_class_info <- function(data, samples) {
 
     # find the timeline
     timeline <- sits_timeline(data)
 
     # precondition is the timeline correct?
-    assertthat::assert_that(length(timeline) > 0,
+    assertthat::assert_that(
+        length(timeline) > 0,
         msg = "sits_timeline_class_info: invalid timeline"
     )
 
@@ -208,8 +227,11 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Test if date fits with the timeline
+#'
 #' @name .sits_timeline_valid_date
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description A timeline is a list of dates where observations are available.
@@ -220,7 +242,9 @@ sits_timeline.classified_image <- function(data) {
 #'
 #' @param date        A date.
 #' @param timeline    A vector of reference dates.
+#'
 #' @return Is this is valid starting date?
+#'
 .sits_timeline_valid_date <- function(date, timeline) {
 
      # is the date inside the timeline?
@@ -251,8 +275,11 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Find dates in the input data cube that match those of the patterns
+#'
 #' @name .sits_timeline_match
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description For correct classification, the input data set
@@ -263,7 +290,9 @@ sits_timeline.classified_image <- function(data) {
 #' @param ref_start_date        reference for starting the classification.
 #' @param ref_end_date          reference for end the classification.
 #' @param num_samples           number of samples.
+#'
 #' @return A list of breaks that will be applied to the input data set.
+#'
 #' @examples
 #' # get a timeline for MODIS data
 #' data("timeline_2000_2017")
@@ -273,6 +302,7 @@ sits_timeline.classified_image <- function(data) {
 #' nsamples <- 23
 #' dates <- sits:::.sits_timeline_match(timeline_2000_2017,
 #'                  ref_start_date, ref_end_date, nsamples)
+#'
 .sits_timeline_match <- function(timeline,
                                  ref_start_date,
                                  ref_end_date,
@@ -297,7 +327,8 @@ sits_timeline.classified_image <- function(data) {
     start_date <- timeline[idx_start_date]
 
     # is the start date a valid one?
-    assertthat::assert_that(.sits_timeline_valid_date(start_date, timeline),
+    assertthat::assert_that(
+        .sits_timeline_valid_date(start_date, timeline),
         msg = ".sits_timeline_match: start date in not inside timeline"
     )
 
@@ -342,8 +373,11 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Find indexes in a timeline that match the reference dates
+#'
 #' @name .sits_timeline_match_indexes
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description For correct classification, the time series of the input data
@@ -354,6 +388,7 @@ sits_timeline.classified_image <- function(data) {
 #'
 #' @param timeline      Timeline of input observations (vector).
 #' @param ref_dates     List of breaks to be applied to the input data set.
+#'
 #' @return              A list of indexes that match the reference dates
 #'                      to the timelines.
 #'
@@ -371,8 +406,11 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Indexes to extract data from a distance table for classification
+#'
 #' @name .sits_timeline_dist_indexes
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @description Given a list of time indexes that indicate
@@ -384,6 +422,7 @@ sits_timeline.classified_image <- function(data) {
 #'
 #' @param  class_info         Tibble with classification information.
 #' @param  ntimes             Number of time instances.
+#'
 #' @return List of indexes to be extracted for each classification interval.
 #'
 .sits_timeline_dist_indexes <- function(class_info, ntimes) {
@@ -421,17 +460,20 @@ sits_timeline.classified_image <- function(data) {
     return(dist_indexes)
 }
 
-
-
 #' @title Create a list of time indexes from the dates index
+#'
 #' @name  .sits_timeline_idx_from_dates
+#'
 #' @keywords internal
+#'
 #' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
 #'
 #' @param  dates_index  A list of dates with the subsets of the input data.
 #' @param  timeline     The timeline of the data set.
 #' @param  bands        Bands used for classification.
+#'
 #' @return              The subsets of the timeline.
+#'
 .sits_timeline_idx_from_dates <- function(dates_index, timeline, bands) {
     # transform the dates index (a list of dates) to a list of indexes
     # this speeds up extracting the distances for classification
@@ -449,14 +491,18 @@ sits_timeline.classified_image <- function(data) {
         })
     return(time_index)
 }
+
 #' @title Find the subset of a timeline that is contained
 #'        in an interval defined by start_date and end_date
+#'
 #' @name  .sits_timeline_during
+#'
 #' @keywords internal
 #'
 #' @param timeline      A valid timeline
 #' @param start_date    A date which encloses the start of timeline
 #' @param end_date      A date which encloses the end of timeline
+#'
 #' @return              A timeline
 #'
 .sits_timeline_during <- function(timeline,
@@ -482,13 +528,16 @@ sits_timeline.classified_image <- function(data) {
 }
 
 #' @title Find if the date information is correct
+#'
 #' @name  .sits_timeline_date_format
+#'
 #' @keywords internal
 #'
 #' @description Given a information about dates, check if the date can be
 #'              interpreted by lubridate
 #'
 #' @param file_info     a tibble with date and band information
+#'
 #' @return              Tibble with corrected date information
 #'
 .sits_timeline_date_format <- function(date_band) {
