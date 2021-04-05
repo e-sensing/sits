4c7d60ab04b15466706e3a05cf06c44b5682d6a7
Rolf Simoes <rolfsimoes@gmail.com>
2021-04-05 17:31:03 -0300

    improve demo classify_raster_xgb

--- demo/classify_raster_xgb.R
+++ demo/classify_raster_xgb.R
@@ -6,61 +6,80 @@ library(randomForest)
 
 if (!requireNamespace("sitsdata", quietly = TRUE)) {
     if (!requireNamespace("devtools", quietly = TRUE)) {
-          install.packages("devtools")
-      }
+        install.packages("devtools")
+    }
     devtools::install_github("e-sensing/sitsdata")
 }
+
+# load the sitsdata library
 library(sitsdata)
 
 # select the bands for classification
-data(br_mt_1_8K_9classes_6bands)
-samples_ndvi_evi <- sits_select(br_mt_1_8K_9classes_6bands, bands = c("EVI", "NDVI"))
+data("samples_matogrosso_mod13q1")
+
+samples_ndvi_evi <- sits_select(
+    data  = samples_matogrosso_mod13q1,
+    bands = c("EVI", "NDVI")
+)
 
 # build the classification model
-xgb_model <- sits_train(samples_ndvi_evi, ml_method = sits_xgboost())
+xgb_model <- sits_train(
+    data      = samples_ndvi_evi,
+    ml_method = sits_xgboost()
+)
 
 # create a data cube to be classified
 # Cube is composed of MOD13Q1 images from the Sinop region in Mato Grosso (Brazil)
 data_dir <- system.file("extdata/sinop", package = "sitsdata")
 sinop <- sits_cube(
-    source = "LOCAL",
-    name = "sinop-2014",
-    satellite = "TERRA",
-    sensor = "MODIS",
-    data_dir = data_dir,
-    delim = "_",
+    source     = "LOCAL",
+    name       = "sinop-2014",
+    satellite  = "TERRA",
+    sensor     = "MODIS",
+    data_dir   = data_dir,
+    delim      = "_",
     parse_info = c("X1", "X2", "band", "date")
 )
 
 # classify the raster image
-sinop_probs <- sits_classify(sinop,
-                             ml_model = xgb_model,
-                             memsize = 8,
-                             multicores = 2,
-                             output_dir = tempdir())
-plot(sinop_probs)
+sinop_probs <- sits_classify(
+    data       = sinop,
+    ml_model   = xgb_model,
+    memsize    = 8,
+    multicores = 2,
+    output_dir = tempdir()
+)
 
 # smooth the result with a Bayesian filter
-sinop_bayes <- sits_smooth(type = "bayes",
-                           sinop_probs,
+sinop_bayes <- sits_smooth(cube       = sinop_probs,
+                           type       = "bayes",
                            output_dir = tempdir())
-plot(sinop_bayes)
 
 # smooth the result with a Gaussian filter
-sinop_gauss <- sits_smooth(type = "gaussian",
-                           sinop_probs,
+sinop_gauss <- sits_smooth(cube       = sinop_probs,
+                           type       = "gaussian",
                            output_dir = tempdir())
-plot(sinop_gauss)
 
 # smooth the result with a bilinear filter
-sinop_bilin <- sits_smooth(type = "bilinear",
-                           sinop_probs,
+sinop_bilin <- sits_smooth(cube       = sinop_probs,
+                           type       = "bilinear",
                            output_dir = tempdir())
+
+sinop_label <- sits_label_classification(
+    cube       = sinop_bayes,
+    output_dir = tempdir()
+)
+
+# plot the probabilities
+plot(sinop_probs)
+
+# plot the smoothened images
+plot(sinop_bayes)
+
+plot(sinop_gauss)
+
 plot(sinop_bilin)
 
-sinop_label <- sits_label_classification(sinop_bayes,
-                                         output_dir = tempdir())
+map_1 <- sits_view(sinop, red = "EVI", green = "NDVI", blue = "EVI", time = 23)
 
-# plot the smoothened image
-map_1 <- plot(sinop, red = "evi", green = "ndvi", blue = "evi", time = 23)
-plot(sinop_label, map = map_1)
+sits_view(sinop_label, map = map_1)
