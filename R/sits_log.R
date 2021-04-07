f51693c02f608e788fa3eef65e7ff4d2fbf79d45
Rolf Simoes <rolfsimoes@gmail.com>
2021-04-07 03:19:31 -0300

    update .sits_log function

--- R/sits_log.R
+++ R/sits_log.R
@@ -1,3 +1,26 @@
+
+#' @title Compute the duration between the last .sits_log call and now
+#' @name .sits_log_elapsed_time
+#' @keywords internal
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#'
+#' @description Compute the difference between now and the last call to
+#' .sits_log function.
+#'
+#' @param time  current time
+#'
+#' @return A character string informing the elapsed time
+.sits_log_elapsed_time <- function(time) {
+
+    if (purrr::is_null(sits_env$last_time_sits_log_call)) {
+        return(NULL)
+    }
+
+    time_elapsed <- difftime(time, sits_env$last_time_sits_log_call)
+
+    return(paste(time_elapsed[[1]], attr(time_elapsed, "units")))
+}
+
 #' @title Log variable content to a file
 #' @name .sits_log
 #' @keywords internal
@@ -7,40 +30,49 @@
 #' session.
 #'
 #' @param output_dir    Output directory to write log file
-#' @param topic         A description to append to time stamp
+#' @param entry         A description to append to time stamp
 #' @param ...           Any valid value to be logged
-.sits_log <- function(output_dir = tempdir(), topic = "", ...) {
+.sits_log <- function(output_dir = tempdir(), entry, ...) {
 
     # if no debug environment variable is set, then exit
     if (Sys.getenv("__SITS_DEBUG__") != TRUE)
         return(invisible(NULL))
 
     # compute elapsed time from last .sits_log call
-    elapsed_time <- .sits_log_elapsed_time()
-
-    # save the last system time on exit
+    time <- Sys.time()
+    elapsed_time <- .sits_log_elapsed_time(time)
     on.exit({
+        # save the last system time on exit
         sits_env$last_time_sits_log_call <- Sys.time()
     }, add = TRUE)
+
+    # output log file
+    log_file = paste0(output_dir, "/", basename(tempdir()), ".log")
+
+    # add date at beginning
+    if (purrr::is_null(elapsed_time)) {
+        cat(paste0("# date: ", format(Sys.time(), format = "%Y-%m-%d"), "\n"),
+            file = log_file, append = TRUE)
+    }
+
+    # write head entry
+    cat(paste0("\n", "# entry: ", entry, "\n"),
+        file = log_file, append = TRUE)
+    cat(paste0("# time: ", format(Sys.time(), format = "%H:%M:%S"), "\n"),
+        file = log_file, append = TRUE)
+
+    # write elapsed time
+    if (!purrr::is_null(elapsed_time)) {
+        cat(paste0("# elapsed time: ", elapsed_time, "\n"),
+            file = log_file, append = TRUE)
+    }
 
     # dots
     values <- list(...)
 
-    # output file
-    log_file = paste0(output_dir, "/", basename(tempdir()), ".log")
-
-    # head
-    time_stamp <- format(x = Sys.time(), format = "[%Y-%m-%dT%H:%M:%S]")
-    topic <- paste(time_stamp, paste0(topic, collapse = " "))
-
-    # write head
-    cat(paste0(topic, "\n"), file = log_file, append = TRUE)
-
-    # write elapsed time
-    if (!purrr::is_null(elapsed_time)) {
-        cat("elapsed_time=\n", file = log_file, append = TRUE)
-        cat(elapsed_time, file = log_file, append = TRUE)
-    }
+    # values section
+    cat("\n",
+        file = log_file, append = TRUE)
 
     # sink output
     sink(file = log_file,
@@ -51,33 +83,13 @@
     # values to be appended
     tryCatch({
         for (i in seq_along(values)) {
-            cat(paste0(names(values)[[i]], "=\n"), append = TRUE)
+            cat(paste0("[", names(values)[[i]], "]\n"),
+                append = TRUE)
             print(values[[i]])
+            cat("\n", append = TRUE)
         }
     }, finally = sink()) # revert sink
-
-    # add a blank line
-    cat("\n", file = log_file, append = TRUE)
 
     return(invisible(NULL))
 }
 
-
-#' @title Compute the duration between the last .sits_log call and now
-#' @name .sits_log_elapsed_time
-#' @keywords internal
-#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
-#'
-#' @description Compute the difference between now and the last call to
-#' .sits_log function.
-#'
-#' @return A character string informing the ellapsed time
-.sits_log_elapsed_time <- function() {
-
-    if (purrr::is_null(sits_env$last_time_sits_log_call))
-        return(NULL)
-
-    time_elapsed <- difftime(Sys.time(), sits_env$last_time_sits_log_call)
-
-    return(paste(time_elapsed[[1]], attr(time_elapsed, "units")))
-}
