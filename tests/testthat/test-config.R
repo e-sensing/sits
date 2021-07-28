40562931c685426914cb6272cbb5f59307f0c6fd
Felipe Carvalho <lipecaso@gmail.com>
2021-07-28 14:56:08 +0000

    update test file

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -1,17 +1,5 @@
 context("Config")
 
-test_that("Internal", {
-    cubes <- sits:::.sits_config_satveg_cubes()
-    expect_true(length(cubes) > 1)
-
-    bbox <- sits:::.sits_config_satveg_bbox(cubes[1])
-    expect_true(bbox["xmin"] < bbox["xmax"])
-
-    expect_true(sits:::.config_memory_bloat() > 1)
-
-    bands <- sits:::.sits_config_satveg_bands()
-})
-
 test_that("Show", {
     con <- file(paste0(tempdir(), "config.txt"))
     writeLines(capture.output(sits_config_show()), con)
