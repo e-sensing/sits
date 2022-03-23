8a1ba461f61f810d96b1efbb5a70721df48aebaa
Gilberto Camara <gilberto.camara.inpe@gmail.com>
2022-03-23 20:06:15 -0300

    lightweight attention encoder

---
+++ R/sits_LightTAE.R
@@ -0,0 +1,248 @@
+#' @title Train a model using Lightweight Temporal Self-Attention Encoder
+#' @name sits_LightTAE
+#'
+#' @author Charlotte Pelletier, \email{charlotte.pelletier@@univ-ubs.fr}
+#' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#'
+#' @description Implementation of Light Temporal Attention Encoder (L-TAE)
+#' for satellite image time series classification.
+#'
+#' This function is based on the paper by Vivien Garnot referenced below
+#' and code available on github at
+#' https://github.com/VSainteuf/lightweight-temporal-attention-pytorch
+#' If you use this method, please cite the original TAE and the LTAE paper.
+#'
+#'
+#' @references
+#' Vivien Garnot, Loic Landrieu, Sebastien Giordano, and Nesrine Chehata,
+#' "Satellite Image Time Series Classification with Pixel-Set Encoders
+#' and Temporal Self-Attention",
+#' 2020 Conference on Computer Vision and Pattern Recognition.
+#'
+#' Vivien Garnot, Loic Landrieu,
+#' "Lightweight Temporal Self-Attention  for Classifying
+#' Satellite Images Time Series",
+#' arXiv preprint arXiv:2007.00586, 2020.
+#'
+#' Schneider, Maja; Körner, Marco,
+#' "[Re] Satellite Image Time Series Classification
+#' with Pixel-Set Encoders and Temporal Self-Attention."
+#' ReScience C 7 (2), 2021.
+#'
+#' @param samples           Time series with the training samples.
+#' @param epochs            Number of iterations to train the model.
+#' @param batch_size        Number of samples per gradient update.
+#' @param validation_split  Number between 0 and 1. Fraction of training data
+#'                          to be used as validation data.
+#' @param verbose           Verbosity mode (0 = silent, 1 = progress bar,
+#'                          2 = one line per epoch).
+#'
+#' @return A fitted model to be passed to \code{\link[sits]{sits_classify}}
+#'
+#' @examples
+#' \dontrun{
+#' # Retrieve the set of samples for the Mato Grosso (provided by EMBRAPA)
+#'
+#' # Build a machine learning model based on deep learning
+#' tae_model <- sits_train(samples_modis_4bands, sits_TAE())
+#' # Plot the model
+#' plot(tae_model)
+#'
+#' # get a point and classify the point with the ml_model
+#' point <- sits_select(point_mt_6bands,
+#'     bands = c("NDVI", "EVI", "NIR", "MIR")
+#' )
+#' class <- sits_classify(point, tae_model)
+#' plot(class, bands = c("NDVI", "EVI"))
+#' }
+#' @export
+sits_LightTAE <- function(samples = NULL,
+                          epochs = 100,
+                          batch_size = 64,
+                          validation_split = 0.2,
+                          verbose = FALSE) {
+
+    # set caller to show in errors
+    .check_set_caller("sits_LightTAE")
+
+    # function that returns torch model based on a sits sample data.table
+    result_fun <- function(data) {
+        # verifies if torch package is installed
+        if (!requireNamespace("torch", quietly = TRUE)) {
+            stop("Please install package torch", call. = FALSE)
+        }
+        # verifies if torch package is installed
+        if (!requireNamespace("luz", quietly = TRUE)) {
+            stop("Please install package luz", call. = FALSE)
+        }
+        # get the labels of the data
+        labels <- sits_labels(data)
+        # create a named vector with integers match the class labels
+        n_labels <- length(labels)
+        int_labels <- c(1:n_labels)
+        names(int_labels) <- labels
+
+        # number of bands and number of samples
+        n_bands <- length(sits_bands(data))
+        n_times <- nrow(sits_time_series(data[1, ]))
+        # timeline of samples
+        timeline <- sits_timeline(data)
+
+        # data normalization
+        stats <- .sits_ml_normalization_param(data)
+        train_data <- .sits_distances(.sits_ml_normalize_data(data, stats))
+
+        # split the data into training and validation data sets
+        # create partitions different splits of the input data
+        test_data <- .sits_distances_sample(train_data,
+                                            frac = validation_split
+        )
+        # remove the lines used for validation
+        train_data <- train_data[!test_data, on = "original_row"]
+        n_samples_train <- nrow(train_data)
+        n_samples_test <- nrow(test_data)
+
+        # shuffle the data
+        train_data <- train_data[sample(
+            nrow(train_data),
+            nrow(train_data)
+        ), ]
+        test_data <- test_data[sample(
+            nrow(test_data),
+            nrow(test_data)
+        ), ]
+        # organize data for model training
+        train_x <- array(
+            data = as.matrix(train_data[, 3:ncol(train_data)]),
+            dim = c(n_samples_train, n_times, n_bands)
+        )
+        train_y <- unname(int_labels[as.vector(train_data$reference)])
+        # create the test data
+        test_x <- array(
+            data = as.matrix(test_data[, 3:ncol(test_data)]),
+            dim = c(n_samples_test, n_times, n_bands)
+        )
+        test_y <- unname(int_labels[as.vector(test_data$reference)])
+
+        # set torch seed
+        torch::torch_manual_seed(sample.int(10^5, 1))
+
+        # define the PSE-TAE model
+        pse_tae_model <- torch::nn_module(
+            classname = "pixel_encoder_light_temporal_attention_encoder",
+            initialize = function(n_bands,
+                                  n_labels,
+                                  timeline,
+                                  dim_input_decoder = 128,
+                                  dim_layers_decoder = c(64, 32)) {
+                # define an spatial encoder
+                self$spatial_encoder <-
+                    .torch_pixel_spatial_enconder(n_bands = n_bands)
+                # define a temporal encoder
+                self$temporal_attention_encoder <-
+                    .torch_temporal_attention_encoder(timeline = timeline)
+
+                # add a final layer to the decoder
+                # with a dimension equal to the number of layers
+                dim_layers_decoder[length(dim_layers_decoder) + 1] <- n_labels
+                self$decoder <- .torch_multi_linear_batch_norm_relu(
+                    dim_input_decoder,
+                    dim_layers_decoder
+                )
+                # classification using softmax
+                self$softmax <- torch::nn_softmax(dim = -1)
+            },
+            forward = function(x){
+                x <- x %>%
+                    self$spatial_encoder() %>%
+                    self$temporal_attention_encoder() %>%
+                    self$decoder() %>%
+                    self$softmax()
+                return(x)
+            }
+        )
+
+        # train the model using luz
+        torch_model <-
+            luz::setup(
+                module = pse_tae_model,
+                loss = torch::nn_cross_entropy_loss(),
+                metrics = list(luz::luz_metric_accuracy()),
+                optimizer = torch::optim_adam
+            ) %>%
+            luz::set_hparams(
+                n_bands  = n_bands,
+                n_labels = n_labels,
+                timeline = timeline
+            ) %>%
+            luz::fit(
+                data = list(train_x, train_y),
+                epochs = epochs,
+                valid_data = list(test_x, test_y),
+                callbacks = list(luz::luz_callback_early_stopping(
+                    patience = 10,
+                    min_delta = 0.05
+                )),
+                verbose = verbose,
+                dataloader_options = list(batch_size = batch_size)
+            )
+
+        model_to_raw <- function(model) {
+            con <- rawConnection(raw(), open = "wr")
+            torch::torch_save(model, con)
+            on.exit(close(con), add = TRUE)
+            r <- rawConnectionValue(con)
+            return(r)
+        }
+
+        model_from_raw <- function(object) {
+            con <- rawConnection(object)
+            on.exit(close(con), add = TRUE)
+            module <- torch::torch_load(con)
+            return(module)
+        }
+        # serialize model
+        serialized_model <- model_to_raw(torch_model$model)
+
+        # construct model predict closure function and returns
+        model_predict <- function(values) {
+
+            # verifies if torch package is installed
+            if (!requireNamespace("torch", quietly = TRUE)) {
+                stop("Please install package torch", call. = FALSE)
+            }
+            # restore model
+            torch_model$model <- model_from_raw(serialized_model)
+
+            # transform input (data.table) into a 3D tensor
+            # remove first two columns
+            # reshape the 2D matrix into a 3D array
+            n_samples <- nrow(values)
+            n_times <- nrow(sits_time_series(data[1, ]))
+            n_bands <- length(sits_bands(data))
+            values_x <- array(
+                data = as.matrix(values[, -2:0]),
+                dim = c(n_samples, n_times, n_bands)
+            )
+            # retrieve the prediction probabilities
+            prediction <- data.table::as.data.table(
+                torch::as_array(
+                    stats::predict(torch_model, values_x)
+                )
+            )
+            # adjust the names of the columns of the probs
+            colnames(prediction) <- labels
+
+            return(prediction)
+        }
+
+        class(model_predict) <- c("torch_model", "sits_model",
+                                  class(model_predict))
+
+        return(model_predict)
+    }
+
+    result <- .sits_factory_function(samples, result_fun)
+    return(result)
+}
--- R/sits_torch_temporal_attention_encoder.R
+++ R/sits_torch_temporal_attention_encoder.R
@@ -254,7 +254,377 @@
         return(o_hat)
     }
 )
+#' @title Torch module for temporal attention encoder
+#' @name .torch_light_temporal_attention_encoder
+#'
+#' @author Charlotte Pelletier, \email{charlotte.pelletier@@univ-ubs.fr}
+#' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#' @author Felipe Souza, \email{lipecaso@@gmail.com}
+#' @keywords internal
+#' @description Defines a torch module for temporal attention encoding.
+#'
+#' This implementation is based on the code made available by Vivien Garnot
+#' https://github.com/VSainteuf/lightweight-temporal-attention-pytorch
+#'
+#' We used the code made available by Maja Schneider in her work with
+#' Marco Körner referenced below and available at
+#' https://github.com/maja601/RC2020-psetae.
+#'
+#' @references
+#' Vivien Sainte Fare Garnot and Loic Landrieu,
+#' "Lightweight Temporal Self-Attention
+#' for Classifying Satellite Image Time Series", https://arxiv.org/abs/2007.00586
+#'
+#' Schneider, Maja; Körner, Marco,
+#' "[Re] Satellite Image Time Series Classification
+#' with Pixel-Set Encoders and Temporal Self-Attention." ReScience C 7 (2), 2021.
+#'
+#' @param timeline                  Timeline of input time series.
+#' @param in_channels               Dimension of the positional encoder.
+#' @param n_heads                   Number of attention heads.
+#' @param n_neurons                 Dimensions of MLP that processes the
+#'                                  output of the attention heads.
+#' @param dropout_rate              Dropout_rate.
+#' @param d_model                   Dimension of the feature space that
+#'                                  the input tensors will projected after
+#'                                  being processed by a fully connected layer.
+#'
+#' @return A linear tensor block.
+#'
+.torch_light_temporal_attention_encoder <- torch::nn_module(
+    classname = "torch_temporal_attention_encoder",
 
+    initialize = function(timeline,
+                          in_channels = 128,
+                          n_heads = 16,
+                          n_neurons = c(256, 128),
+                          dropout_rate = 0.2,
+                          d_model = 256){
+        # store parameters
+        self$in_channels <-  in_channels
+        self$n_heads     <-  n_heads
+        # calculate the dimension of split enconder
+        self$d_k <-  in_channels %/% n_heads
+        # calculate the length of the time sequence
+        self$seq_len <- length(timeline)
+        # Do a 1D convolution on the input sequence
+        self$d_model <- d_model
+        self$inconv  <- torch::nn_sequential(
+            torch::nn_conv1d(in_channels, self$d_model, 1),
+            torch::nn_layer_norm(list(self$d_model, self$seq_len))
+        )
+        # obtain the positional encoding
+        self$pos_encoding <-  .torch_positional_encoding(
+            timeline = timeline,
+            dim_encoder = in_channels
+        )
+
+        # input layer normalization
+        self$in_layer_norm <- torch::nn_layer_norm(self$in_channels)
+
+        # output layer normalization
+        last_neuron <- n_neurons[length(n_neurons)]
+        self$out_layer_norm <- torch::nn_layer_norm(last_neuron)
+
+        # calculate the attention heads
+        self$attention_heads = .torch_multi_head_attention(
+            n_heads   = n_heads,
+            d_k       = self$dim_k,
+            d_in      = self$d_model
+            )
+
+        # multi-layer perceptron with batch norm and relu
+        hidden_dims = n_neurons[-1]
+        self$mlp  <- .torch_multi_linear_batch_norm_relu(
+            input_dim = n_neurons[1],
+            hidden_dims = hidden_dims
+        )
+    },
+    forward = function(x){
+        # Follows figure 2 of Garnot's paper
+        # "Satellite Image Time Series Classification
+        # with Pixel-Set Encoders and Temporal Self-Attention"
+        #
+        # obtain the input parameters
+        batch_size   <- x$shape[[1]]
+        # seq_len is the size of the timeline
+        seq_len      <- x$shape[[2]]
+        # in_channels is the size of the transformed input sequence
+        in_channels  <- x$shape[[3]]
+
+        # normalize the input layer
+        # [batch_size x seq_len x in_channels:128]
+        x <- self$in_layer_norm(x)
+        # reshape the tensor
+        # from [batch_size x seq_len x in_channels:128]
+        # to [batch_size x in_channels:128 x seq_len]
+        x <- x$permute(c(1,3,2))
+        # Apply a 1D convolution to the permuted normalized input
+        # shape [batch_size x in_channels:128 x seq_len]
+        x <- self$inconv()
+        # Reshape the tensor
+        # from [batch_size x in_channels:128 x seq_len]
+        # to [batch_size x seq_len x in_channels:128]
+        x <- x$permute(c(1,3,2))
+
+        # Calculate the positional encoding
+        # result is 3D shape [batch_size x seq_len x in_channels:128]
+        e_p <-  self$pos_encoding(x)
+
+        # Calculate the query and key tensors
+        # FC1 is the fully-connected layer generating key-query pairs
+        # for each head
+
+        # Calculate the query tensor
+        # Run the encoded position through FC1
+        # input is a tensor of shape [batch_size x seq_len x dim_encoder:128]
+        # result is a tensor of shape [batch_size x seq_len x dim_encoder:128]
+        q <-  self$fc1_q(e_p)
+        # Calculate the mean of query tensor along dimension 2
+        # result is a tensor of shape [batch_size x dim_encoder:128]
+        q_mean <-  torch::torch_mean(q, dim = 2)
+        # Run the mean by a FC2 (fully connected layer 2)
+        q_hat <- self$fc2(q_mean)
+        # Reorganize the result as a 3D tensor
+        # result is a tensor [batch_size x n_heads:4 x dim_k:32]
+        q_hat <- q_hat$view(c(batch_size, self$n_heads, self$dim_k))
+        q_hat <- q_hat$contiguous()
+        # Reorganize the result as a 2D tensor
+        # output shape is 2D [(batch_size * n_heads:4) x dim_k:32]
+        q_hat <- q_hat$view(c(-1, self$dim_k))
+        # Create an additional dimension
+        # output shape is 3D [(batch_size * n_heads:4) x 1 x dim_k:32]
+        q_hat <- q_hat$unsqueeze(dim = 2)
+
+        # Calculate the key tensor
+        # Run the encoded position through FC1
+        # result is a tensor of shape [batch_size x seq_len x dim_encoder:128]
+        k <-  self$fc1_k(e_p)
+        # Reorganize the tensor as a 4D shape
+        # shape is 4D [batch_size x seq_len x n_heads:4 x dim_k:32]
+        k <- k$view(c(batch_size, seq_len, self$n_heads, self$dim_k))
+        # Permute dimensions (2,3) of the 4D tensor
+        k <- k$permute(c(1, 3, 2, 4))
+        # shape is 4D [batch_size x n_heads:4 x seq_len x dim_k:32]
+        k <- k$contiguous()
+        # Reduce the key tensor to 3D merging dimensions (1,2)
+        # shape is 3D [(batch_size * n_heads:4) x seq_len x dim_k:32]
+        k <- k$view(c(batch_size *  self$n_heads, seq_len, self$dim_k))
+
+        # transpose key tensor dimensions 2 and 3
+        # input shape is 3D [(batch_size * n_heads) x seq_len x dim_k]
+        # output shape is 3D [(batch_size * n_heads) x dim_k x seq_len]
+        k <- torch::torch_transpose(k, dim0 = -2, dim1 = -1)
+
+        # Calculate attention
+        # Attention scores =  averaged product of query and key tensor
+        # q_hat has shape 3D [(batch_size * n_heads) x 1 x dim_k]
+        # k transposed has 3D shape  [(batch_size * n_heads) x dim_k x seq_len]
+        # output scores has 3D shape [(batch_size * n_heads) x 1 x seq_len]
+        attention_scores = torch::torch_matmul(q_hat, k)/sqrt(self$dim_k)
+
+        # Attention probs are calculated as
+        # softmax of the normalized query * key product using the last dimension
+        # input shape is 3D  [(batch_size * n_heads) x 1 x seq_len]
+        # output_shape is 3D [(batch_size * n_heads) x 1 x seq_len]
+        attention_probs = torch::nnf_softmax(attention_scores, dim = -1)
+
+        # Values (position encoding)
+        # input 3D shape [batch_size x seq_len x hidden_state:128]
+        # output 3D shape [(batch_size * num_heads) x seq_len x hidden:128]
+        values = e_p$`repeat`(c(self$n_heads, 1, 1))
+
+        # Multi-head self-attention
+        # multiply values by product of query * key
+        # attention_probs - 3D shape [(batch_size * n_heads) x 1 x seq_len]
+        # v - 3D shape [(batch_size * num_heads) x seq_len x hidden:128]
+        # result has 3D shape [(batch_size * num_heads) x 1 x hidden:128]
+        attention_output = torch::torch_matmul(attention_probs, values)
+
+        # squeeze attention output
+        # input shape 3D [(batch_size * n_heads) x 1 x hidden:128]
+        # output shape 2D [(batch_size * n_heads) x hidden:128]
+        attention_output <- torch::torch_squeeze(attention_output)
+
+        # reshape attention output to 3D shape
+        # input shape is 2D [(batch_size * n_heads) x hidden:128]
+        # output shape is 3D [batch_size x n_heads x hidden_state:128]
+        attention_output = attention_output$contiguous()
+        attention_output = attention_output$view(c(batch_size, self$n_heads, -1))
+
+        # reshape attention output to 2D shape
+        # input shape is 3D [batch_size x n_heads x dim_encoder:128]
+        # output shape is 2D [batch_size x (n_heads:4 * dim_encoder:128)]
+        attention_output = attention_output$contiguous()
+        attention_output = attention_output$view(c(batch_size, -1))
+
+        # Run the output by a multi-layer perceptron
+        # input shape is 2D [batch_size x (n_heads:4 * dim_encoder:128)]
+        # output shape is 2D [batch_size x dim_encoder:128]
+        o_hat = self$mlp3(attention_output)
+        return(o_hat)
+    }
+)
+#' @title Torch module for calculating attention from query, keys and values
+#' @name .torch_scaled_dot_product_attention
+#'
+#' @author Charlotte Pelletier, \email{charlotte.pelletier@@univ-ubs.fr}
+#' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#' @author Felipe Souza, \email{lipecaso@@gmail.com}
+#' @keywords internal
+#' @description Defines a torch module for temporal attention encoding.
+#'
+#' In order to calculate attentions with a query, as I said in the last article,
+#' this function takes the dot product of ‘query’ with the ‘keys’
+#' and gets scores/weights for the ‘values.’
+#' Each score/weight the relevance between the ‘query’ and each ‘key’.
+#' And you reweight the ‘values’ with the scores/weights,
+#' and take the summation of the reweighted ‘values.
+#'
+#' This implementation is based on the code made available by Vivien Garnot
+#' https://github.com/VSainteuf/lightweight-temporal-attention-pytorch
+#'
+#' @references
+#' Vivien Sainte Fare Garnot and Loic Landrieu,
+#' "Lightweight Temporal Self-Attention
+#' for Classifying Satellite Image Time Series", https://arxiv.org/abs/2007.00586
+#'
+#'
+#' @param temperature               Weight score of the attention module.
+#' @param attn_dropout              Dropout rate to be applied to the attention module.
+#' @param query                     Query tensor.
+#' @param keys                      Tensor with keys.
+#' @param values                    Tensor with values.
+#'
+#' @return A list with the .
+#'
+.torch_scaled_dot_product_attention <- torch::nn_module(
+    classname = "scaled_dot_product_attention",
+    initialize = function(temperature, attn_dropout = 0.1){
+        self$temperature <-  temperature
+        self$dropout <-  torch::nn_dropout(attn_dropout)
+        self$softmax <- torch::nn_softmax(dim = 3)
+    },
+    forward = function(query, keys, values){
+        # calculate the dot product between query and keys
+        # query has 2D shape [(n_heads * batch_size) x d_k]
+        # after unsqueezing a 3D shape [(n_heads * batch_size) x 1 x d_k]
+        # keys has 3D shape [(n_heads * batch_size) x  seq_len x d_k]
+        # after transpose a 3D shape [(n_heads * batch_size) x d_k x seq_len]
+        attn <- torch::torch_matmul(
+            query$unsqueeze(dim = 2),
+            keys$transpose(dim0 = 2, dim1 = 3)
+        )
+        # attention tensor has 3D shape [(n_heads * batch_size) x 1 x seq_len]
+        # weight the attention module
+        attn <- attn / self$temperature
+        # apply a software layer to the weighted query * keys product
+        attn <- self$softmax(attn)
+        # apply a dropout value to the attention tensor
+        attn <- self$dropout(attn)
+        # calculate the product attention * values
+        # split_value <- dim_encoder %/% n_heads
+        # attention tensor has 3D shape [(n_heads * batch_size) x 1 x seq_len]
+        # values has 3D shape [(num_heads * batch_size) x seq_len x split_value]
+        # output has a 3D shape [(num_heads * batch_size) x 1 x split_value]
+        output <- torch::torch_matmul(attn, values)
+
+        return(list(output = output, attn = attn))
+    }
+)
+
+
+.torch_multi_head_attention <- torch::nn_module(
+    classname = "multi_head_attention",
+    initialize = function(n_heads, d_k, d_in){
+        self$n_heads <- n_heads
+        self$d_k     <- d_k
+        self$d_in    <- d_in
+
+        # create a base vector for queries
+        # shape [n_heads x d_k]
+        self$Q <- torch::nn_parameter(
+            torch::torch_zeros(c(n_heads, d_k), requires_grad = TRUE)
+        )
+        # change self$Q in-place
+        torch::nn_init_normal_(self$Q, mean = 0, std = sqrt(2.0 / (d_k)))
+
+        self$fc_k <- torch::nn_linear(d_in, n_heads * d_k)
+        torch::nn_init_normal_(self$fc_k$weight, mean = 0, std = sqrt(2.0 / (d_k)))
+
+        self$attention <- .torch_scaled_dot_product_attention(
+            temperature = sqrt(d_k)
+        )
+    },
+    forward = function(query, key, values){
+        d_k     <- self$d_k
+        d_in    <- self$d_in
+        n_heads <- self$n_heads
+
+        # query tensor is of
+        batch_size <- query$size[[1]]
+        seq_len    <- query$size[[2]]
+
+        # calculate the query tensor
+        # concatenate a sequence of tensors to match input batch_size
+        tensors <- purrr::map(seq_len(batch_size), function(i){
+            return(self$Q)
+        })
+        # the query tensor has shape [batch_size x n_heads x d_k]
+        query <- torch::torch_stack(tensors, dim = 2)
+        # drop a dimension in the query tensor
+        # from 3D shape [n_heads x batch_size x d_k]
+        # to 2D shape [(n_heads * batch_size) x d_k]
+        query <- query$view(c(-1, d_k))
+
+        # calculate the keys tensor
+        # tensor has 4D shape [batch_size, seq_len, n_heads, d_k]
+        keys = self$fc_k(values)$view(c(batch_size, seq_len, n_heads, d_k))
+        # permute shape of keys tensor
+        # from 4D shape [batch_size, seq_len, n_heads, d_k]
+        # to 4D shape [n_heads, batch_size, seq_len, d_k]
+        keys = keys$permute(c(3, 1, 2, 4))$contiguous()
+        # Reshape keys tensor to 3D [(n_heads * batch_size) x  seq_len x d_k]
+        keys <- keys$view(c(-1, seq_len, d_k))
+
+        # calculate the values tensor
+        # original tensor from positional encoding
+        # has 3D shape [batch_size x seq_len x dim_encoder:128]
+        dim_encoder <- values$shape[length(values$shape)]
+        # reshape the values tensor
+        # from 3D shape[batch_size x seq_len x  dim_encoder:128]
+        split_value <- dim_encoder %/% n_heads
+        # to 4D shape [split_value x seq_len x dim_encoder x  (batch_size %/% split_value)]
+        values <- torch::torch_stack(values$split(split_value), dim = -1)
+        # reshape the values tensor
+        # from 4D shape [split_value x seq_len x dim_encoder x  (batch_size %/% split_value)]
+        # to 3D shape [(num_heads * batch_size) x seq_len x split_value]
+        values <- values$view(c(n_heads * batch_size, seq_len, -1))
+        # calculate the attention values
+        output_attn_lst <- self$attention(query, keys, values)
+        # recover the attention tensor
+        attn <- output_attn_lst$attn
+        # reshape the attention tensor
+        # from 3D shape [(n_heads * batch_size) x 1 x seq_len]
+        # to 4D shape [n_heads x batch_size x 1 x seq_len]
+        attn <- attn$view(c(n_heads, batch_size, 1, seq_len))
+        # squeeze the attention tensor on the third dimension
+        # result is a 3D shape [n_heads x batch_size x seq_len]
+        attn <- attn$squeeze(dim = 3)
+        #  retrieve the output tensor
+        output <- output_attn_lst$output
+        # output has 3D shape [(num_heads * batch_size) x seq_len x split_value]
+        # d_in = 256 and n_heads = 16, d_in %/% n_heads = 16
+        # reshape to 4D shape [num_heads, batch_size x 1 x dim_encoder]
+        output = output$view(c(n_heads, batch_size, 1, d_in %/% n_heads))
+        output = output$squeeze(dim = 3)
+
+        return(list(output = output, attn = attn))
+    }
+
+)
 
 
 
