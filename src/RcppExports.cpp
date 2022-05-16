2fdc87654807d9fa8fff0440c38fd33efe631ed2
albhasan <albhasan@gmail.com>
2022-05-16 09:31:45 -0300

    Add metric 'ratio of confidence' to sits uncertainty.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -128,6 +128,18 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// ratio_probs
+IntegerVector ratio_probs(const IntegerMatrix& mtx, const int& n);
+RcppExport SEXP _sits_ratio_probs(SEXP mtxSEXP, SEXP nSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const IntegerMatrix& >::type mtx(mtxSEXP);
+    Rcpp::traits::input_parameter< const int& >::type n(nSEXP);
+    rcpp_result_gen = Rcpp::wrap(ratio_probs(mtx, n));
+    return rcpp_result_gen;
+END_RCPP
+}
 // smooth_whit
 NumericVector smooth_whit(const NumericVector& data, const double& lambda, const int& length);
 RcppExport SEXP _sits_smooth_whit(SEXP dataSEXP, SEXP lambdaSEXP, SEXP lengthSEXP) {
@@ -152,6 +164,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_sits_linear_interp_vec", (DL_FUNC) &_sits_linear_interp_vec, 1},
     {"_sits_margin_probs", (DL_FUNC) &_sits_margin_probs, 2},
     {"_sits_normalize_data", (DL_FUNC) &_sits_normalize_data, 3},
+    {"_sits_ratio_probs", (DL_FUNC) &_sits_ratio_probs, 2},
     {"_sits_smooth_whit", (DL_FUNC) &_sits_smooth_whit, 3},
     {NULL, NULL, 0}
 };
