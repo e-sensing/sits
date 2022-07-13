2a59b3176cca179fd45ca410fd837c12335af20a
Rolf Simoes <rolfsimoes@gmail.com>
2022-07-13 19:31:50 -0300

    fix #809

--- R/sits_som.R
+++ R/sits_som.R
@@ -193,7 +193,11 @@ sits_som_map <- function(data,
 
     # prepare a color assignment to the SOM map
     kohonen_obj$neuron_label <- labels_max
-    kohonen_obj <- .sits_som_paint_neurons(kohonen_obj)
+
+    # only paint neurons if number of labels is greater than one
+    if (length(unique(labels_max)) > 1)
+        kohonen_obj <- .sits_som_paint_neurons(kohonen_obj)
+
     # return the som_map object
     som_map <-
         list(
