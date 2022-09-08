dd975ca243b0031b168021e18cc5f717e2652a41
Rolf Simoes <rolfsimoes@gmail.com>
2022-09-07 23:41:11 -0300

    improve sits jobs api

--- R/sits_jobs.R
+++ R/sits_jobs.R
@@ -1,98 +1,63 @@
-.jobs_max_multicores <- function(job_ncols,
-                                 job_nrows,
-                                 npaths,
-                                 nbytes,
-                                 proc_bloat,
-                                 overlap,
-                                 memsize,
-                                 multicores) {
-    # Memory needed per block
-    job_memsize <- (job_nrows + 2 * overlap) * (job_ncols + 2 * overlap) *
-        npaths * nbytes * proc_bloat * 1e-09
+
+.jobs_memsize <- function(job_size, npaths, nbytes, proc_bloat, overlap) {
+    # Memory needed per job
+    (job_size[["nrows"]] + 2 * overlap) *
+        (job_size[["ncols"]] + 2 * overlap) * npaths *
+        nbytes * proc_bloat * 1e-09
+}
+
+.jobs_max_multicores <- function(job_memsize, memsize, multicores) {
     # Max parallel blocks supported by memsize
     max_blocks <- floor(memsize / job_memsize)
-    # Check if memsize is above minimum needed to process one block
-    .check_that(
-        x = max_blocks > 0,
-        local_msg = paste("minimum memsize needed is",
-                          job_memsize, "GB"),
-        msg = "provided 'memsize' is insufficient for processing"
-    )
     # Max multicores
-    return(min(multicores, max_blocks))
+    min(multicores, max_blocks)
 }
-.jobs_create_job <- function(col,
-                             row,
-                             ncols,
-                             nrows,
-                             max_ncols,
-                             max_nrows,
-                             overlap,
-                             template_obj) {
-    # Start job creation
-    job <- tibble::tibble(
-        col = as.integer(max(1, col - overlap)),
-        row = as.integer(max(1, row - overlap))
-    )
-    job[["ncols"]] = as.integer(
-        min(job[["col"]] + ncols + overlap - 1, max_ncols) - job[["col"]] + 1
-    )
-    job[["nrows"]] = as.integer(
-        min(job[["row"]] + nrows + overlap - 1, max_nrows) - job[["row"]] + 1
-    )
-    job[["overlap"]] = as.integer(overlap)
-    # Crop block from template
-    r_obj <- .raster_crop_metadata(
-        r_obj = template_obj,
-        block = c(first_col = col,
-                  first_row = row,
-                  ncols = job[["ncols"]],
-                  nrows = job[["nrows"]])
-    )
-    # Add bbox information
-    job[["xmin"]] = .raster_xmin(r_obj = r_obj)
-    job[["xmax"]] = .raster_xmax(r_obj = r_obj)
-    job[["ymin"]] = .raster_ymin(r_obj = r_obj)
-    job[["ymax"]] = .raster_ymax(r_obj = r_obj)
-    job[["crs"]] = .raster_crs(r_obj = r_obj)
-    return(job)
-}
-.jobs_create <- function(block_ncols,
-                         block_nrows,
-                         block_overlap = 0,
-                         ncols,
-                         nrows,
-                         xmin,
-                         xmax,
-                         ymin,
-                         ymax,
-                         crs,
-                         roi = NULL) {
+
+.jobs_create <- function(job_size, block_overlap, ncols, nrows, xmin, xmax,
+                         ymin, ymax, crs, roi = NULL) {
     # Prepare raster tile template
     template_obj <- .raster_new_rast(
-        nrows = nrows,
-        ncols = ncols,
-        xmin = xmin,
-        xmax = xmax,
-        ymin = ymin,
-        ymax = ymax,
-        nlayers = 1,
-        crs = crs
+        nrows = nrows, ncols = ncols, xmin = xmin, xmax = xmax,
+        ymin = ymin, ymax = ymax, nlayers = 1, crs = crs
     )
     # Generate (col, row) for all blocks
-    cols_rows <- purrr::cross_df(list(
-        col = seq(1, ncols - block_overlap, block_ncols),
-        row = seq(1, nrows - block_overlap, block_nrows)
+    cr <- purrr::cross_df(list(
+        col = seq(1, ncols - block_overlap, job_size[["ncols"]]),
+        row = seq(1, nrows - block_overlap, job_size[["nrows"]])
     ))
     # Create all jobs
-    jobs <- purrr::pmap(cols_rows, .jobs_create_job,
-        ncols = block_ncols,
-        nrows = block_nrows,
-        max_ncols = ncols,
-        max_nrows = nrows,
-        overlap = block_overlap,
-        template = template_obj
-    )
+    jobs <- purrr::pmap(cr, function(col, row, ncols, nrows, max_ncols,
+                                     max_nrows, overlap, template_obj) {
+        # Start job creation
+        job <- tibble::tibble(
+            col = as.integer(max(1, col - overlap)),
+            row = as.integer(max(1, row - overlap))
+        )
+        job[["ncols"]] = as.integer(
+            min(job[["col"]] + ncols + overlap - 1, max_ncols) -
+                job[["col"]] + 1
+        )
+        job[["nrows"]] = as.integer(
+            min(job[["row"]] + nrows + overlap - 1, max_nrows) -
+                job[["row"]] + 1
+        )
+        job[["overlap"]] = as.integer(overlap)
+        # Crop block from template
+        r_obj <- .raster_crop_metadata(
+            r_obj = template_obj,
+            block = c(first_col = col, first_row = row, ncols = job[["ncols"]],
+                      nrows = job[["nrows"]])
+        )
+        # Add bbox information
+        job[["xmin"]] = .raster_xmin(r_obj = r_obj)
+        job[["xmax"]] = .raster_xmax(r_obj = r_obj)
+        job[["ymin"]] = .raster_ymin(r_obj = r_obj)
+        job[["ymax"]] = .raster_ymax(r_obj = r_obj)
+        job[["crs"]] = .raster_crs(r_obj = r_obj)
+        job
+    }, ncols = job_size[["ncols"]], nrows = job_size[["nrows"]],
+    max_ncols = ncols, max_nrows = nrows, overlap = block_overlap,
+    template = template_obj)
     # If no roi was provided return all jobs
     if (is.null(roi)) {
         return(jobs)
@@ -101,6 +66,106 @@
     jobs <- .jobs_intersects(jobs = jobs, roi = roi)
     return(jobs)
 }
+
+.jobs_read_tile_eo <- function(job, tile, ml_model, impute_fn, filter_fn) {
+    # Convert job to block
+    block <- .jobs_as_block(job)
+    # Get file_info
+    fi <- .fi(tile)
+    # Get cloud values
+    cloud_mask <- .fi_read_block(fi = fi, band = .band_cloud(), block = block)
+    # Prepare cloud_mask
+    if (!is.null(cloud_mask)) {
+        cloud_mask <- .values_cloud_mask(
+            values = cloud_mask,
+            interp_values = .tile_cloud_interp_values(tile),
+            is_bit_mask = .tile_cloud_bit_mask(tile)
+        )
+    }
+    # Get model bands
+    bands <- .ml_model_bands(ml_model)
+    # Get bands values
+    values <- purrr::map(bands, function(band) {
+        # Get band values
+        values <- .fi_read_block(fi = fi, band = band, block = block)
+        # Remove cloud masked pixels
+        if (!is.null(cloud_mask)) values[cloud_mask] <- NA
+        # Correct missing, minimum, and maximum values; and scale values
+        # Get band defaults for derived cube from config
+        conf_band <- .conf_eo_band(
+            source = .tile_source(tile), collection = .tile_collection(tile),
+            band = band
+        )
+        # This can be ignored as it is already process by gdal
+        # miss_value = .band_miss_value(conf_band)
+        # if (!is.null(miss_value)) {
+        #     values[values == miss_value] <- NA
+        # }
+        min_value <- .band_min_value(conf_band)
+        if (!is.null(min_value)) {
+            values[values < min_value] <- NA
+        }
+        max_value <- .band_max_value(conf_band)
+        if (!is.null(max_value)) {
+            values[values > max_value] <- NA
+        }
+        # Remove NA pixels
+        if (!is.null(impute_fn)) {
+            values <- impute_fn(values)
+        }
+        offset <- .band_offset(conf_band)
+        if (!is.null(offset)) {
+            values <- values - offset
+        }
+        scale <- .band_scale(conf_band)
+        if (!is.null(scale)) {
+            values <- values * scale
+        }
+        # Filter the time series
+        if (!(is.null(filter_fn))) {
+            values <- filter_fn(values)
+        }
+        # Normalize values
+        stats <- .ml_model_stats(ml_model)
+        if (!is.null(stats$q02) && !is.null(stats$q98)) {
+            values <- normalize_data(values, stats$q02, stats$q98)
+        }
+        values
+    })
+    values <- do.call(cbind, values)
+    colnames(values) <- .ml_model_attr_names(ml_model)
+    values
+}
+
+.jobs_write_values <- function(job, values, data_type, file_pattern,
+                               output_dir) {
+    # create a new raster
+    r_obj <- .raster_new_rast(
+        nrows = job[["nrows"]], ncols = job[["ncols"]],
+        xmin = job[["xmin"]], xmax = job[["xmax"]],
+        ymin = job[["ymin"]], ymax = job[["ymax"]],
+        nlayers = ncol(values), crs = job[["crs"]]
+    )
+    # copy values
+    r_obj <- .raster_set_values(
+        r_obj = r_obj,
+        values = values
+    )
+    # Get output file name
+    file <- .file_block_name(
+        pattern = file_pattern, block = .jobs_as_block(job),
+        output_dir = output_dir
+    )
+    # write the probabilities to a raster file
+    .raster_write_rast(
+        r_obj = r_obj,
+        file = file,
+        data_type = data_type,
+        overwrite = TRUE
+    )
+    file
+}
+
 .jobs_intersects <- function(jobs, roi) {
     # Preconditions
     .check_that(
@@ -130,20 +195,7 @@
     })
     return(jobs)
 }
-# .jobs_process <- function(job, files, fn) {
-#     .raster_read_stack(
-#         files = files,
-#         block = .jobs_as_block(job)
-#     )
-# }
-# .jobs_process <- function(job, ..., fn) {
-#     chunk_lst <- purrr::map(
-#         list(...), .raster_read_stack,
-#         block = .jobs_as_block(job)
-#     )
-#     res <- fn(chunk_lst)
-#
-# }
+
 .jobs_as_block <- function(job) {
     dplyr::transmute(
         job,
@@ -154,37 +206,10 @@
     )
 }
 .jobs_as_sf <- function(job) {
-    .bbox_as_sf(
-        xmin = job[["xmin"]],
-        xmax = job[["xmax"]],
-        ymin = job[["ymin"]],
-        ymax = job[["ymax"]],
-        crs = job[["crs"]]
-    )
+    .bbox_as_sf(job)
 }
-.job_save_values <- function(job, values, data_type, out_file) {
-    # create a new raster
-    r_obj <- .raster_new_rast(
-        nrows = job[["nrows"]],
-        ncols = job[["ncols"]],
-        xmin = job[["xmin"]],
-        xmax = job[["xmax"]],
-        ymin = job[["ymin"]],
-        ymax = job[["ymax"]],
-        nlayers = ncol(values),
-        crs = job[["crs"]]
-    )
-    # copy values
-    r_obj <- .raster_set_values(
-        r_obj = r_obj,
-        values = values
-    )
-    # write the probabilities to a raster file
-    .raster_write_rast(
-        r_obj = r_obj,
-        file = out_file,
-        format = "GTiff",
-        data_type = data_type,
-        overwrite = TRUE
-    )
+
+.jobs_parallel_chr <- function(jobs, fn, ...) {
+    values_lst <- .sits_parallel_map(jobs, fn, ..., progress = TRUE)
+    vapply(values_lst, c, NA_character_)
 }
