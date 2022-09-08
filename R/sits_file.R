bd71bdb9e1b3d7a3d2d13ffbcdc0feeb0da2632b
Rolf Simoes <rolfsimoes@gmail.com>
2022-09-07 23:35:00 -0300

    improve code style

--- R/sits_file.R
+++ R/sits_file.R
@@ -2,31 +2,21 @@
 .file_block_name <- function(pattern, block, output_dir) {
     # Get output_dir
     .file_path(
-        pattern, "block", block[["first_row"]],
-        block[["first_col"]],
-        ext = "tif",
-        output_dir = file.path(output_dir, ".sits"),
+        pattern, "block", block[["first_row"]], block[["first_col"]],
+        ext = "tif", output_dir = file.path(output_dir, ".sits"),
         create_dir = TRUE
     )
 }
 
 .file_probs_name <- function(tile, version, output_dir) {
     .file_path(
-        tile[["satellite"]],
-        tile[["sensor"]],
-        tile[["tile"]],
-        .tile_start_date(tile),
-        .tile_end_date(tile),
-        .config_get("probs_cube_band"),
-        version,
-        ext = "tif",
-        output_dir = output_dir
+        tile[["satellite"]], tile[["sensor"]], .tile_name(tile),
+        .tile_start_date(tile), .tile_end_date(tile), "probs", version,
+        ext = "tif", output_dir = output_dir
     )
 }
 
-.file_path <- function(...,
-                       ext = NULL,
-                       output_dir = NULL,
+.file_path <- function(..., ext = NULL, output_dir = NULL,
                        create_dir = FALSE) {
     filenames <- paste(..., sep = "_")
     if (!is.null(ext)) {
