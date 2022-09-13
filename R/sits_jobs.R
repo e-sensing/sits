a349272e142d0c78ae9f2240f71cac082b0a2ee2
Felipe <lipecaso@gmail.com>
2022-09-13 04:54:04 +0000

    update jobs api

--- R/sits_jobs.R
+++ R/sits_jobs.R
@@ -22,8 +22,8 @@
     )
     # Generate (col, row) for all blocks
     cr <- purrr::cross_df(list(
-        col = seq(1, ncols - block_overlap, job_size[["ncols"]]),
-        row = seq(1, nrows - block_overlap, job_size[["nrows"]])
+        col = seq(1, ncols, job_size[["ncols"]]),
+        row = seq(1, nrows, job_size[["nrows"]])
     ))
     # Create all jobs
     jobs <- purrr::pmap(cr, function(col, row, ncols, nrows, max_ncols,
@@ -33,27 +33,29 @@
             col = as.integer(max(1, col - overlap)),
             row = as.integer(max(1, row - overlap))
         )
-        job[["ncols"]] = as.integer(
-            min(job[["col"]] + ncols + overlap - 1, max_ncols) -
-                job[["col"]] + 1
+        job[["ncols"]] <- as.integer(
+            min(max_ncols, col + ncols + overlap - 1) - job[["col"]] + 1
+
         )
-        job[["nrows"]] = as.integer(
-            min(job[["row"]] + nrows + overlap - 1, max_nrows) -
-                job[["row"]] + 1
+        job[["nrows"]] <- as.integer(
+            min(max_nrows, row + nrows + overlap - 1) - job[["row"]] + 1
         )
-        job[["overlap"]] = as.integer(overlap)
+        job[["overlap"]] <- as.integer(overlap)
+        job[["crop_ncols"]] <- as.integer(min(max_ncols - col + 1, ncols))
+        job[["crop_nrows"]] <- as.integer(min(max_nrows - row + 1, nrows))
         # Crop block from template
         r_obj <- .raster_crop_metadata(
             r_obj = template_obj,
-            block = c(col = col, row = row, ncols = job[["ncols"]],
+            block = c(col = job[["col"]], row = job[["row"]],
+                      ncols = job[["ncols"]],
                       nrows = job[["nrows"]])
         )
         # Add bbox information
-        job[["xmin"]] = .raster_xmin(r_obj = r_obj)
-        job[["xmax"]] = .raster_xmax(r_obj = r_obj)
-        job[["ymin"]] = .raster_ymin(r_obj = r_obj)
-        job[["ymax"]] = .raster_ymax(r_obj = r_obj)
-        job[["crs"]] = .raster_crs(r_obj = r_obj)
+        job[["xmin"]] <- .raster_xmin(r_obj = r_obj)
+        job[["xmax"]] <- .raster_xmax(r_obj = r_obj)
+        job[["ymin"]] <- .raster_ymin(r_obj = r_obj)
+        job[["ymax"]] <- .raster_ymax(r_obj = r_obj)
+        job[["crs"]] <- .raster_crs(r_obj = r_obj)
         job
     }, ncols = job_size[["ncols"]], nrows = job_size[["nrows"]],
     max_ncols = ncols, max_nrows = nrows, overlap = block_overlap,
