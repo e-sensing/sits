15b66513d77f6442f95b11834de598b20f769efc
Felipe <lipecaso@gmail.com>
2022-09-19 01:49:44 +0000

    update src document

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -230,18 +230,18 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// nnls_solver
-arma::mat nnls_solver(const arma::mat x, const arma::mat A, const bool rmse, const int iterate, const float tolerance);
-RcppExport SEXP _sits_nnls_solver(SEXP xSEXP, SEXP ASEXP, SEXP rmseSEXP, SEXP iterateSEXP, SEXP toleranceSEXP) {
+// C_nnls_solver
+arma::mat C_nnls_solver(const arma::mat& x, const arma::mat& em, const bool rmse, const int max_it, const float tol);
+RcppExport SEXP _sits_C_nnls_solver(SEXP xSEXP, SEXP emSEXP, SEXP rmseSEXP, SEXP max_itSEXP, SEXP tolSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< const arma::mat >::type x(xSEXP);
-    Rcpp::traits::input_parameter< const arma::mat >::type A(ASEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type x(xSEXP);
+    Rcpp::traits::input_parameter< const arma::mat& >::type em(emSEXP);
     Rcpp::traits::input_parameter< const bool >::type rmse(rmseSEXP);
-    Rcpp::traits::input_parameter< const int >::type iterate(iterateSEXP);
-    Rcpp::traits::input_parameter< const float >::type tolerance(toleranceSEXP);
-    rcpp_result_gen = Rcpp::wrap(nnls_solver(x, A, rmse, iterate, tolerance));
+    Rcpp::traits::input_parameter< const int >::type max_it(max_itSEXP);
+    Rcpp::traits::input_parameter< const float >::type tol(tolSEXP);
+    rcpp_result_gen = Rcpp::wrap(C_nnls_solver(x, em, rmse, max_it, tol));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -378,7 +378,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_sits_C_label_max_prob", (DL_FUNC) &_sits_C_label_max_prob, 1},
     {"_sits_linear_interp", (DL_FUNC) &_sits_linear_interp, 1},
     {"_sits_linear_interp_vec", (DL_FUNC) &_sits_linear_interp_vec, 1},
-    {"_sits_nnls_solver", (DL_FUNC) &_sits_nnls_solver, 5},
+    {"_sits_C_nnls_solver", (DL_FUNC) &_sits_C_nnls_solver, 5},
     {"_sits_normalize_data", (DL_FUNC) &_sits_normalize_data, 3},
     {"_sits_max_sampling", (DL_FUNC) &_sits_max_sampling, 5},
     {"_sits_smooth_sg", (DL_FUNC) &_sits_smooth_sg, 4},
