9ea8b93a9f401486548dc4e8786dc6f4f6928aba
Felipe <lipecaso@gmail.com>
2022-09-22 02:01:24 +0000

    change features to assets in sits_cube_copy

--- R/sits_cube_copy.R
+++ R/sits_cube_copy.R
@@ -21,65 +21,60 @@ sits_cube_copy <- function(cube,
     # Prepare parallel processing
     .sits_parallel_start(workers = multicores, log = FALSE)
     on.exit(.sits_parallel_stop(), add = TRUE)
-    # Create features as jobs
-    features <- .cube_feature_create(cube)
+    # Create assets as jobs
+    assets <- .cube_asset_create(cube)
     # Process each tile sequentially
-    features <- .jobs_map_sequential_dfr(features, function(feature) {
-        local_tile <- .download_feature(
-            feature = feature, res = res,
+    assets <- .jobs_map_parallel_dfr(assets, function(asset) {
+        local_tile <- .download_asset(
+            asset = asset, res = res,
             roi = roi, output_dir = output_dir,
             progress = progress
         )
     })
-    # Join output features as a cube and return it
-    .cube_merge_features(features)
+    # Join output assets as a cube and return it
+    .cube_merge_assets(assets)
 }
 
-.download_feature <- function(feature, res, roi, output_dir, progress) {
+.download_asset <- function(asset, res, roi, output_dir, progress) {
     # Get all paths and expand
-    files <- path.expand(.fi_paths(.fi(feature)))
+    file <- path.expand(.fi_paths(.fi(asset)))
     # Create a list of user parameters as gdal format
-    gdal_params <- .gdal_format_params(feature = feature, roi = roi, res = res)
-    # Start parallel downloading
-    destfiles <- .jobs_map_parallel_chr(files, function(file) {
-        # Create output file
-        out_file <- file.path(output_dir, .file_base(file))
-        # Resume feature
-        if (.raster_is_valid(out_file)) {
-            return(out_file)
-        }
-        # Get a gdal or default download
-        download_fn <- .download_controller(out_file, gdal_params)
-        # Download file
-        out_file <- download_fn(file)
-        # Return the destination file path
-        out_file
-    })
-
+    gdal_params <- .gdal_format_params(asset = asset, roi = roi, res = res)
+    # Create output file
+    out_file <- file.path(output_dir, .file_base(file))
+    # Resume feature
+    if (.raster_is_valid(out_file)) {
+        return(out_file)
+    }
+    # Get a gdal or default download
+    download_fn <- .download_controller(out_file, gdal_params)
+    # Download file
+    out_file <- download_fn(file)
+    # Update asset metadata
     update_bbox <- if (is.null(roi) && is.null(res)) FALSE else TRUE
-    feature <- .tile_eo_from_files(
-        files = destfiles, fid = .fi_fid(.fi(feature)),
-        bands = .fi_bands(.fi(feature)), date = .tile_start_date(feature),
-        base_tile = feature, update_bbox = update_bbox
+    asset <- .tile_eo_from_files(
+        files = out_file, fid = .fi_fid(.fi(asset)),
+        bands = .fi_bands(.fi(asset)), date = .tile_start_date(asset),
+        base_tile = asset, update_bbox = update_bbox
     )
-    return(feature)
+    asset
 }
 
-.gdal_format_params <- function(feature, roi, res) {
+.gdal_format_params <- function(asset, roi, res) {
     gdal_params <- list()
     if (!is.null(res)) {
         gdal_params[["tr"]] <- c(res, res)
     }
     if (!is.null(roi)) {
-        gdal_params[["srcwin"]] <- .gdal_as_srcwin(feature = feature, roi = roi)
+        gdal_params[["srcwin"]] <- .gdal_as_srcwin(asset = asset, roi = roi)
     }
     gdal_params[c("of", "co")] <- list("GTiff", .config_gtiff_default_options())
 
     gdal_params
 }
 
-.gdal_as_srcwin <- function(feature, roi) {
-    block <- .sits_raster_sub_image(tile = feature, roi = roi)
+.gdal_as_srcwin <- function(asset, roi) {
+    block <- .sits_raster_sub_image(tile = asset, roi = roi)
     c(xoff = block[["col"]] - 1,
       yoff = block[["row"]] - 1,
       xsize = block[["ncols"]],
@@ -87,29 +82,29 @@ sits_cube_copy <- function(cube,
     )
 }
 
-.download_controller <- function(destfile, gdal_params) {
+.download_controller <- function(out_file, gdal_params) {
     # gdal is used if the image needs to be cropped or resampled
     if (any(c("srcwin", "tr") %in% names(gdal_params))) {
-        download_fn <- .download_gdal(destfile, gdal_params)
+        download_fn <- .download_gdal(out_file, gdal_params)
     } else {
-        download_fn <- .download_base(destfile)
+        download_fn <- .download_base(out_file)
     }
     return(download_fn)
 }
 
-.download_gdal <- function(destfile, gdal_params) {
+.download_gdal <- function(out_file, gdal_params) {
     download_fn <- function(file) {
-        gdal_params[c("src_dataset", "dst_dataset")] <- list(file, destfile)
+        gdal_params[c("src_dataset", "dst_dataset")] <- list(file, out_file)
         do.call(
             what = gdalUtilities::gdal_translate,
             args = gdal_params
         )
-        destfile
+        out_file
     }
     download_fn
 }
 
-.download_base <- function(destfile) {
+.download_base <- function(out_file) {
     donwload_fn <- function(file) {
         # Add file scheme in path
         if (.file_is_local(file)) {
@@ -117,9 +112,9 @@ sits_cube_copy <- function(cube,
         }
         download.file(
             url = .file_remove_vsi(file),
-            destfile = destfile, quiet = TRUE
+            destfile = out_file, quiet = TRUE
         )
-        destfile
+        out_file
     }
     donwload_fn
 }
