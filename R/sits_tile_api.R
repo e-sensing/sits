f323006d011416d6c7a08388d8852daae7ee93a1
Rolf Simoes <rolfsimoes@gmail.com>
2022-10-11 18:53:05 -0300

    Change function name .fi_date() -> .fi_timeline()

--- R/sits_tile_api.R
+++ R/sits_tile_api.R
@@ -1539,13 +1539,6 @@ NULL
     )
 }
 
-.fi_date <- function(fi) {
-    .fi_switch(
-        fi = fi,
-        eo_cube = .as_date(fi[["date"]][[1]])
-    )
-}
-
 .fi_timeline <- function(fi) {
     .fi_switch(
         fi = fi,
