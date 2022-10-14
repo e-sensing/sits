946e0825db2f4ed33b510799b4201f43458adba9
Rolf Simoes <rolfsimoes@gmail.com>
2022-10-14 03:35:33 -0300

    Improve code style

--- R/sits_select.R
+++ R/sits_select.R
@@ -59,11 +59,11 @@ sits_select.sits_cube <- function(data, bands, ..., tiles = NULL) {
 
     # filter tiles
     if (!is.null(tiles)) {
-        data <- .cube_filter_tile(data, tiles)
+        data <- .cube_filter_tiles(cube = data, tiles = tiles)
     }
 
     # filter bands
-    data <- ..cube_filter_bands(data, bands)
+    data <- .cube_filter_bands(cube = data, bands = bands)
 
     return(data)
 }
