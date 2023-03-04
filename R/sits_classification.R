bcbf41bf00d66d20d0d8e50769e9e5a53244579a
Rolf Simoes <rolfsimoes@gmail.com>
2023-03-04 19:00:23 -0300

    Improve code style

--- R/sits_classification.R
+++ R/sits_classification.R
@@ -161,7 +161,8 @@ sits_classify.raster_cube <- function(data,
 
     # Spatial filter
     if (.has(roi)) {
-        data <- .cube_filter_spatial(cube = data, roi = .roi_as_sf(roi))
+        roi <- .roi_as_sf(roi)
+        data <- .cube_filter_spatial(cube = data, roi = roi)
     }
     # Temporal filter
     if (.has(start_date) || .has(end_date)) {
