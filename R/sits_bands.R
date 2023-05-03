50fa58ba84ccc68f5317e83e49b98524f3a27407
Rolf Simoes <rolfsimoes@gmail.com>
2023-05-03 18:11:03 -0300

    Implement band renaming

--- R/sits_bands.R
+++ R/sits_bands.R
@@ -15,6 +15,8 @@
 #'
 #' @examples
 #' bands <- sits_bands(point_mt_6bands)
+#' bands[[5]] <- "EVI2"
+#' sits_bands(point_mt_6bands) <- bands
 #'
 #' @export
 sits_bands <- function(x) {
@@ -56,4 +58,43 @@ sits_bands.sits_model <- function(x) {
     .check_is_sits_model(x)
     bands <- .ml_bands(x)
     return(bands)
+}
+
+#' @rdname sits_bands
+#' @export
+`sits_bands<-` <- function(x, value) {
+    # Get the meta-type (sits or cube)
+    x <- .conf_data_meta_type(x)
+    UseMethod("sits_bands<-", x)
+}
+
+#' @rdname sits_bands
+#' @export
+`sits_bands<-.sits` <- function(x, value) {
+    bands <- sits_bands(x)
+    .check_that(
+        length(bands) == length(value),
+        local_msg = paste0("bands must have length ", length(bands)),
+        msg = "invalid band list"
+    )
+    x <- .apply(x, col = "time_series", fn = function(x) {
+        names(x) <- c("Index", value, "#..")
+        return(x)
+    })
+    return(x)
+}
+#' @rdname sits_bands
+#' @export
+`sits_bands<-.raster_cube` <- function(x, value) {
+    bands <- sits_bands(x)
+    .check_that(
+        length(bands) == length(value),
+        local_msg = paste0("bands must have length ", length(bands)),
+        msg = "invalid band list"
+    )
+    x <- slider::slide_dfr(x, function(tile) {
+        .tile_bands(tile) <- value
+        tile
+    })
+    return(x)
 }
