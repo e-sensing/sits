185f5cce83ceccd3ab2cb51978041e46d9c25459
Felipe <lipecaso@gmail.com>
2023-06-17 19:13:46 +0000

    update sits_clean function

--- R/sits_clean.R
+++ R/sits_clean.R
@@ -51,7 +51,8 @@ sits_clean <- function(cube,
             clean_fn = clean_fn,
             window_size = window_size,
             overlap = overlap,
-            output_dir = output_dir
+            output_dir = output_dir,
+            version = version
         )
         return(output_asset)
     }, progress = progress)
@@ -64,6 +65,91 @@ sits_clean <- function(cube,
                          clean_fn,
                          window_size,
                          overlap,
-                         output_dir) {
+                         output_dir,
+                         version) {
+    # Output file
+    out_file <- .file_derived_name(
+        tile = asset, band = out_band,
+        version = version, output_dir = output_dir
+    )
+    # Resume asset
+    if (.raster_is_valid(out_file, output_dir = output_dir)) {
+        # recovery message
+        .check_recovery(out_file)
 
+        # Create tile based on template
+        # TODO: check this
+        asset <- .tile_derived_from_file(
+            file = out_file, band = out_band,
+            base_tile = asset, update_bbox = FALSE
+        )
+        return(asset)
+    }
+    # Remove remaining incomplete fractions files
+    unlink(out_file)
+    # Create chunks as jobs
+    chunks <- .tile_chunks_create(
+        tile = asset, overlap = overlap, block = block
+    )
+    # Process jobs sequentially
+    block_files <- .jobs_map_sequential(chunks, function(chunk) {
+        # Get job block
+        block <- .block(chunk)
+        # Block file name for each fraction
+        block_files <- .file_block_name(
+            pattern = .file_pattern(out_file), block = block,
+            output_dir = output_dir
+        )
+        # Resume processing in case of failure
+        if (.raster_is_valid(block_files)) {
+            return(block_files)
+        }
+        # Read bands data
+        values <- .clean_data_read(
+            tile = asset, block = block
+        )
+        # Evaluate expression here
+        # Band and kernel evaluation
+        values <- clean_fn(values)
+        # Prepare fractions to be saved
+        band_conf <- .tile_band_conf(tile = asset, band = out_band)
+        # Job crop block
+        crop_block <- .block(.chunks_no_overlap(chunk))
+        # Prepare and save results as raster
+        .raster_write_block(
+            files = block_files, block = block, bbox = .bbox(chunk),
+            values = values, data_type = .data_type(band_conf),
+            missing_value = .miss_value(band_conf),
+            crop_block = crop_block
+        )
+        # Free memory
+        gc()
+        # Returned block files for each fraction
+        block_files
+    })
+    # Merge blocks into a new class_cube tile
+    band_tile <- .tile_derived_merge_blocks(
+        file = out_file,
+        bands = out_band,
+        labels = labels,
+        base_tile = asset,
+        derived_class = .tile_derived_class(asset),
+        block_files = block_files,
+        multicores = 1,
+        update_bbox = FALSE
+    )
+    # Return a asset
+    band_tile
 }
+
+.clean_data_read <- function(tile, block) {
+    # Get band values
+    band <- .tile_bands(tile)
+    values <- .tile_read_block(tile = tile, band = band, block = block)
+    values <- as.data.frame(values)
+    # Set columns name
+    colnames(values) <- band
+    # Return values
+    values
+}
+
