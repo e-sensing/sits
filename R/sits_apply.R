6c08c1a4057cd6a9650955d98089baff7f1a97a3
Gilberto Camara <gilberto.camara@inpe.br>
2023-12-26 14:26:06 -0300

    avoid warnings in sits_apply when doing documentation

--- R/sits_apply.R
+++ R/sits_apply.R
@@ -135,10 +135,13 @@ sits_apply.raster_cube <- function(data, ...,
     out_band <- names(expr)
     # Check if band already exists in cube
     if (out_band %in% .cube_bands(data)) {
-        warning(
-            paste0("The provided band '", out_band, "' already exists in cube."),
-            call. = FALSE
-        )
+        if (.check_messages()){
+            warning(
+                paste0("The provided band '", out_band,
+                       "' already exists in cube."),
+                call. = FALSE
+            )
+        }
         return(data)
     }
     # Get all input bands in cube data
