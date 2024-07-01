db2937c69c480737e3db07a625c54a7549f7a073
Felipe <lipecaso@gmail.com>
2024-07-01 23:57:04 +0000

    update base cube function

--- R/api_cube.R
+++ R/api_cube.R
@@ -135,6 +135,12 @@ NULL
     # return the cube
     x
 }
+
+.cube_base_info <- function(x) {
+    # return base info
+    dplyr::bind_rows(x[["base_info"]])
+}
+
 #' @title Return areas of classes of a class_cue
 #' @keywords internal
 #' @noRd
@@ -192,7 +198,7 @@ NULL
         class(cube) <- c("raster_cube", class(cube))
         bands <- .cube_bands(cube)
     } else {
-        stop(.conf("messages", "cube_bands"))
+        stop(.conf("messages", ".cube_bands"))
     }
     return(bands)
 }
@@ -203,7 +209,7 @@ NULL
         cube <- tibble::as_tibble(cube)
         bands <- .cube_bands(cube, add_cloud, dissolve)
     } else {
-        stop(.conf("messages", "cube_bands"))
+        stop(.conf("messages", ".cube_bands"))
     }
     return(bands)
 }
@@ -544,6 +550,17 @@ NULL
     }
     return(is_regular)
 }
+
+#' @title Check that cube is a base cube
+#' @name .cube_is_base
+#' @keywords internal
+#' @noRd
+#' @param cube  datacube
+#' @return Called for side effects.
+.cube_is_base <- function(cube) {
+    inherits(cube, "base_raster_cube")
+}
+
 #' @title Find out how many images are in cube during a period
 #' @noRd
 #' @param cube  A data cube.
--- R/sits_add_base_cube.R
+++ R/sits_add_base_cube.R
@@ -55,13 +55,13 @@
 #' }
 #' @export
 #'
-sits_add_base_cube <- function(cube1, cube2){
+sits_add_base_cube <- function(cube1, cube2) {
     .check_set_caller("sits_add_base_cube")
     .check_is_raster_cube(cube1)
     .check_that(.cube_is_regular(cube1))
     .check_that(inherits(cube2, "dem_cube"))
     # pre-condition for merge is having the same tiles
-    .check_that(all(cube1[["tile"]] %in% cube2[["tile"]]))
+    .check_cubes_same_tiles(cube1, cube2)
     # extract tiles
     tiles <- .cube_tiles(cube1)
     # add base info by tile
