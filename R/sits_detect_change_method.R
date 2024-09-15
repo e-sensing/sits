34a544249ba92a182f09963a287dd54361ba66e7
Felipe Carlos <efelipecarlos@gmail.com>
2024-09-15 06:56:58 -0300

    review dtw for detect changes

--- R/sits_detect_change_method.R
+++ R/sits_detect_change_method.R
@@ -12,20 +12,21 @@
 #' @return                  Change detection method prepared
 #'                          to be passed to
 #'                          \code{\link[sits]{sits_detect_change}}
+#' @export
 sits_detect_change_method <- function(samples, cd_method = sits_dtw()) {
     # set caller to show in errors
     .check_set_caller("sits_detect_change_method")
     # check if samples are valid
-    # .check_samples_train(samples)
+    .check_samples_train(samples)
     # is the train method a function?
     .check_that(inherits(cd_method, "function"),
                 msg = .conf("messages", "sits_detect_change_method_model")
     )
     # are the timelines OK?
-    # timeline_ok <- .timeline_check(samples)
-    # .check_that(timeline_ok,
-    #             msg = .conf("messages", "sits_detect_change_method_timeline")
-    # )
+    timeline_ok <- .timeline_check(samples)
+    .check_that(timeline_ok,
+                 msg = .conf("messages", "sits_detect_change_method_timeline")
+    )
     # compute the training method by the given data
     result <- cd_method(samples)
     # return a valid detect change method
