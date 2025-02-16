552a2d6753d3935ad1d383884baf27388f453535
Felipe <lipecaso@gmail.com>
2025-02-16 22:51:32 +0000

    working in progress on sits_glcm

---
+++ R/api_glcm.R
@@ -0,0 +1,192 @@
+#' @title Apply an expression to block of a set of input bands
+#' @name .apply_feature
+#' @keywords internal
+#' @noRd
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#' @author Felipe Carvalho, \email{rolf.simoes@@inpe.br}
+#'
+#' @param  feature         Subset of a data cube containing the input bands
+#'                         used in the expression
+#' @param  block           Individual block that will be processed
+#' @param  window_size     Size of the neighbourhood (if required)
+#' @param  angles          ...
+#' @param  expr            Expression to be applied
+#' @param  out_band        Output band
+#' @param  in_bands        Input bands
+#' @param  overlap         Overlap between tiles (if required)
+#' @param  normalized      Produce normalized band?
+#' @param  output_dir      Directory where image will be save
+#'
+#' @return                 A feature compose by a combination of tile and band.
+.glcm_feature <- function(feature, block, window_size, angles, expr,
+                          out_band, in_bands, overlap, output_dir) {
+    # Output file
+    out_file <- .file_eo_name(
+        tile = feature, band = out_band,
+        date = .tile_start_date(feature), output_dir = output_dir
+    )
+    # Resume feature
+    if (.raster_is_valid(out_file, output_dir = output_dir)) {
+        # recovery message
+        .check_recovery(out_file)
+
+        # Create tile based on template
+        feature <- .tile_eo_from_files(
+            files = out_file, fid = .fi_fid(.fi(feature)),
+            bands = out_band, date = .tile_start_date(feature),
+            base_tile = feature, update_bbox = FALSE
+        )
+        return(feature)
+    }
+    # Remove remaining incomplete fractions files
+    unlink(out_file)
+    # Create chunks as jobs
+    chunks <- .tile_chunks_create(
+        tile = feature, overlap = overlap, block = block
+    )
+    # Get band configuration
+    band_conf <- .tile_band_conf(tile = feature, band = out_band)
+    if (.has_not(band_conf)) {
+        band_conf <- .conf("default_values", "INT4S")
+    }
+    # Process jobs sequentially
+    block_files <- .jobs_map_sequential(chunks, function(chunk) {
+        # Get job block
+        block <- .block(chunk)
+        # Block file name for each fraction
+        block_files <- .file_block_name(
+            pattern = .file_pattern(out_file),
+            block = block,
+            output_dir = output_dir
+        )
+        # Resume processing in case of failure
+        if (.raster_is_valid(block_files)) {
+            return(block_files)
+        }
+        # Read bands data
+        # TODO: create glcm data read
+        values <- .apply_data_read(
+            tile = feature, block = block, in_bands = in_bands
+        )
+        # ...
+        values <- values * 10000
+        values <- .as_int(c(values)[[1]])
+
+        # Evaluate expression here
+        # Band and kernel evaluation
+        # values <- eval(
+        #     expr = expr[[out_band]],
+        #     envir = values,
+        #     enclos = .glcm_functions(
+        #         window_size = window_size,
+        #         angles = angles,
+        #         img_nrow = block[["nrows"]],
+        #         img_ncol = block[["ncols"]]
+        #     )
+        # )
+        # TODO: rescale value
+        values <-  C_glcm_variance(
+            x = values, nrows = block[["nrows"]], ncols = block[["ncols"]],
+            window_size = window_size, angles = 0
+        )
+        # Prepare fractions to be saved
+        offset <- .offset(band_conf)
+        if (.has(offset) && offset != 0) {
+            values <- values - offset
+        }
+        # Job crop block
+        crop_block <- .block(.chunks_no_overlap(chunk))
+        # Prepare and save results as raster
+        .raster_write_block(
+            files = block_files, block = block, bbox = .bbox(chunk),
+            values = values, data_type = .data_type(band_conf),
+            missing_value = .miss_value(band_conf),
+            crop_block = crop_block
+        )
+        # Free memory
+        gc()
+        # Returned block files for each fraction
+        block_files
+    })
+    # Merge blocks into a new eo_cube tile
+    band_tile <- .tile_eo_merge_blocks(
+        files = out_file,
+        bands = out_band,
+        band_conf = band_conf,
+        base_tile = feature,
+        block_files = block_files,
+        multicores = 1,
+        update_bbox = FALSE
+    )
+    # Return a feature tile
+    band_tile
+}
+
+#' @title Kernel function for window operations in spatial neighbourhoods
+#' @name .glcm_functions
+#' @noRd
+#' @param windows size of local window
+#' @param img_nrow  image size in rows
+#' @param img_ncol  image size in cols
+#' @return operations on local kernels
+#'
+.glcm_functions <- function(window_size, angles, img_nrow, img_ncol) {
+    result_env <- list2env(list(
+        glcm_contrast = function(m) {
+            C_glcm_contrast(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_dissimilarity = function(m) {
+            C_glcm_dissimilarity(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_homogeneity = function(m) {
+            C_glcm_homogeneity(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_energy = function(m) {
+            C_glcm_energy(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_asm = function(m) {
+            C_glcm_asm(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_mean = function(m) {
+            C_glcm_mean(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_variance = function(m) {
+            C_glcm_variance(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_std = function(m) {
+            C_glcm_std(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        },
+        glcm_correlation = function(m) {
+            C_glcm_correlation(
+                x = m, nrows = img_nrow, ncols = img_ncol,
+                window_size = window_size, angles = angles
+            )
+        }
+    ), parent = parent.env(environment()), hash = TRUE)
+
+    return(result_env)
+}
---
+++ R/sits_glcm.R
@@ -0,0 +1,181 @@
+#' @title Apply a GLCM metric on a data cube
+#'
+#' @name sits_glcm
+#'
+#' @author Rolf Simoes, \email{rolf.simoes@@inpe.br}
+#' @author Felipe Carvalho, \email{felipe.carvalho@@inpe.br}
+#' @author Gilberto Camara, \email{gilberto.camara@@inpe.br}
+#'
+#' @description ...
+#'
+#' @param data          Valid sits tibble or cube
+#' @param window_size   An odd number representing the size of the
+#'                      sliding window of sits kernel functions
+#'                      used in expressions (for a list of supported
+#'                      kernel functions, please see details).
+#' @param angles        ...
+#' @param memsize       Memory available for classification (in GB).
+#' @param multicores    Number of cores to be used for classification.
+#' @param output_dir    Directory where files will be saved.
+#' @param progress      Show progress bar?
+#' @param ...           Named expressions to be evaluated (see details).
+#'
+#' @section Summarizing GLCM functions:
+#' \itemize{
+#' \item{\code{glcm_contrast()}: ...}
+#' \item{\code{glcm_dissimilarity()}: ...}
+#' \item{\code{glcm_homogeneity()}: ...}
+#' \item{\code{glcm_energy()}: ...}
+#' \item{\code{glcm_asm()}: ...}
+#' \item{\code{glcm_mean()}: ...}
+#' \item{\code{glcm_variance()}: ...}
+#' \item{\code{glcm_std()}: ...}
+#' \item{\code{glcm_correlation()}: ...}
+#' }
+#'
+#' @return A sits cube with new bands, produced
+#'         according to the requested expression.
+#'
+#' @examples
+#' if (sits_run_examples()) {
+#'     data_dir <- system.file("extdata/raster/mod13q1", package = "sits")
+#'     cube <- sits_cube(
+#'         source = "BDC",
+#'         collection = "MOD13Q1-6.1",
+#'         data_dir = data_dir
+#'     )
+#'
+#'     # Generate a texture images with variance in NDVI images
+#'     cube_texture <- sits_apply(
+#'         data = cube,
+#'         NDVIMEAN = glcm_mean(NDVI),
+#'         window_size = 5,
+#'         angles = 0,
+#'         output_dir = tempdir()
+#'     )
+#' }
+#' @rdname sits_glcm
+#' @export
+sits_glcm <- function(data, ...) {
+    .check_set_caller("sits_glcm")
+    .check_na_null_parameter(data)
+    UseMethod("sits_glcm", data)
+}
+
+#' @rdname sits_glcm
+#' @export
+sits_glcm.raster_cube <- function(data, ...,
+                                  window_size = 3L,
+                                  angles = c(0, pi/2, pi/4, 3*pi/4),
+                                  memsize = 4L,
+                                  multicores = 2L,
+                                  output_dir,
+                                  progress = FALSE) {
+    # Check cube
+    .check_is_raster_cube(data)
+    .check_that(.cube_is_regular(data))
+    # Check window size
+    .check_int_parameter(window_size, min = 1, is_odd = TRUE)
+    # Check normalized index
+    .check_num_parameter(angles)
+    # Check memsize
+    .check_int_parameter(memsize, min = 1, max = 16384)
+    # Check multicores
+    .check_int_parameter(multicores, min = 1, max = 2048)
+    # Check output_dir
+    .check_output_dir(output_dir)
+
+    # Get cube bands
+    bands <- .cube_bands(data)
+    # Get output band expression
+    expr <- .apply_capture_expression(...)
+    out_band <- names(expr)
+    # Check if band already exists in cube
+    if (out_band %in% bands) {
+        if (.check_messages()) {
+            warning(.conf("messages", "sits_apply_out_band"),
+                    call. = FALSE
+            )
+        }
+        return(data)
+    }
+    # Get all input bands in cube data
+    in_bands <- .apply_input_bands(
+        cube = data,
+        bands = bands,
+        expr = expr
+    )
+    # Overlapping pixels
+    overlap <- ceiling(window_size / 2) - 1
+    # Get block size
+    block <- .raster_file_blocksize(.raster_open_rast(.tile_path(data)))
+    # Check minimum memory needed to process one block
+    job_memsize <- .jobs_memsize(
+        job_size = .block_size(block = block, overlap = overlap),
+        npaths = length(in_bands) + 1,
+        nbytes = 8,
+        proc_bloat = .conf("processing_bloat_cpu")
+    )
+    # Update block parameter
+    block <- .jobs_optimal_block(
+        job_memsize = job_memsize,
+        block = block,
+        image_size = .tile_size(.tile(data)),
+        memsize = memsize,
+        multicores = multicores
+    )
+    # adjust for blocks of size 1
+    block <- .block_regulate_size(block)
+    # Update multicores parameter
+    multicores <- .jobs_max_multicores(
+        job_memsize = job_memsize,
+        memsize = memsize,
+        multicores = multicores
+    )
+    # Prepare parallelization
+    .parallel_start(workers = multicores)
+    on.exit(.parallel_stop(), add = TRUE)
+
+    # Create features as jobs
+    features_cube <- .cube_split_features(data)
+
+    # Process each feature in parallel
+    features_band <- .jobs_map_parallel_dfr(features_cube, function(feature) {
+        # Process the data
+        output_feature <- .glcm_feature(
+            feature = feature,
+            block = block,
+            expr = expr,
+            window_size = window_size,
+            angles = angles,
+            out_band = out_band,
+            in_bands = in_bands,
+            overlap = overlap,
+            output_dir = output_dir
+        )
+        return(output_feature)
+    }, progress = progress)
+    # Join output features as a cube and return it
+    .cube_merge_tiles(dplyr::bind_rows(list(features_cube, features_band)))
+}
+
+#' @rdname sits_glcm
+#' @export
+sits_glcm.derived_cube <- function(data, ...) {
+    stop(.conf("messages", "sits_glcm_derived_cube"))
+}
+#' @rdname sits_glcm
+#' @export
+sits_glcm.default <- function(data, ...) {
+    data <- tibble::as_tibble(data)
+    if (all(.conf("sits_cube_cols") %in% colnames(data))) {
+        data <- .cube_find_class(data)
+    } else if (all(.conf("sits_tibble_cols") %in% colnames(data))) {
+        class(data) <- c("sits", class(data))
+    } else {
+        stop(.conf("messages", "sits_glcm_default"))
+    }
+
+    acc <- sits_glcm(data, ...)
+    return(acc)
+}
