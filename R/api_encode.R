0b3e6fe645f0261b5c095f0b115c1b125cf6e799
Rolf Simoes <rolfsimoes@gmail.com>
2026-07-14 11:16:08 -0300

    Document new functions

--- R/api_encode.R
+++ R/api_encode.R
@@ -1095,6 +1095,20 @@
         update_bbox = update_bbox
     )
 }
+#' @title Encode a chunk of data on CPU
+#' @author Rolf Simoes, \email{rolfsimoes@@gmail.com}
+#' @keywords internal
+#' @noRd
+#' @param  chunk        Chunk to be processed
+#' @param  tile         Input data tile
+#' @param  base_bands  Base bands
+#' @param  bands        Bands to be used
+#' @param  band_conf    Band configuration
+#' @param  impute_fn    Imputation function
+#' @param  filter_fn    Filter function
+#' @param  output_dir   Output directory
+#' @param  out_files    Output files
+#' @return              Block file path
 .encode_chunk_cpu <- function(chunk,
                               tile,
                               base_bands,
