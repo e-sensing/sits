71fff84ab451446ddceb94420402611def1c2a5f
Rolf Simoes <rolfsimoes@gmail.com>
2026-07-25 04:25:24 -0300

    Fix dataloader pipeline

--- R/sits_barlow_twins.R
+++ R/sits_barlow_twins.R
@@ -307,15 +307,9 @@ sits_barlow_twins <- function(samples          = NULL,
                 model = torch_model$model,
                 raw = serialized_model
             )
-            # GPU or CPU classification?
-            use_gpu <- (
-                is.list(values) &&
-                    !is.null(values[["callback"]]) &&
-                    .torch_gpu_classification()
-            )
-            # Encode!
-            if (use_gpu) {
-                # Predict!
+            # Use dataloader?
+            if (.torch_use_dataloader(values)) {
+                # Encode!
                 values <- .torch_predict_chunks(
                     torch_model = torch_model,
                     dataset = values[["dataset"]],
--- R/sits_tae.R
+++ R/sits_tae.R
@@ -346,14 +346,8 @@ sits_tae <- function(samples = NULL,
                 model = torch_model$model,
                 raw = serialized_model
             )
-            # GPU or CPU classification?
-            use_gpu <- (
-                is.list(values) &&
-                    !is.null(values[["callback"]]) &&
-                    .torch_gpu_classification()
-            )
-            # Classify!
-            if (use_gpu) {
+            # Use dataloader?
+            if (.torch_use_dataloader(values)) {
                 # Predict!
                 values <- .torch_predict_chunks(
                     torch_model = torch_model,
